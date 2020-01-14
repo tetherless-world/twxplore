dce9bb2050bf567c6cf0230739a1d8a961f155c3
Minor Gordon <gordom6@rpi.edu>
2020-01-14 14:26:45 -0500

    geo service running

--- service/geo/test/stores/TestStore.scala
+++ service/geo/test/stores/TestStore.scala
@@ -4,4 +4,5 @@ import models.domain.Feature
 
 object TestStore extends Store {
   override def getFeatures(limit: Int, offset: Int): List[Feature] = if (offset == 0) List(TestData.feature) else List()
+  override def getFeaturesCount() = 1
 }
