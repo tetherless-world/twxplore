3e3e0cd6854b99e3580306f84c9c53a3d2d00ddb
Minor Gordon <gordom6@rpi.edu>
2020-01-15 13:39:22 -0500

    script to load data

---
+++ devbin/load-dsa-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+DSA_DIR=$PWD/../../dsa_whyis
+TWKS_DIR=$PWD/../../twks
+
+java -Dtwks.serverBaseUrl=http://twks-server:8080 -jar $TWKS_DIR/java/dist/twks-cli-current.jar \
+  put-nanopublications \
+      $DSA_DIR/ontologies/prov-o.ttl \
+      $DSA_DIR/ontologies/sio-subset-labels.owl \
+      $DSA_DIR/ontologies/geosparql_vocab_all_v1_0_1_updated.rdf \
+      $DSA_DIR/data/mil.ttl.gz \
+      $DSA_DIR/data/state.ttl.gz \
+      $DSA_DIR/ontologies/dsa-t.ttl \
+      $DSA_DIR/ontologies/dsa-g.ttl \
+      $DSA_DIR/ontologies/dsa-owl.ttl \
+      $DSA_DIR/ontologies/dsa_lgs-rpi_v2b.ttl
