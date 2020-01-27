891a6af898f98f59dff80c030e086fc08e5dc54a
Minor Gordon <gordom6@rpi.edu>
2020-01-27 11:06:07 -0500

    bootstrap CLI projects

---
+++ cli/tree/src/main/scala/edu/rpi/tw/twxplore/cli/tree/TreeCli.scala
@@ -0,0 +1,40 @@
+package edu.rpi.tw.twxplore.cli.tree
+
+import com.beust.jcommander.{JCommander, Parameter}
+import edu.rpi.tw.twxplore.cli.tree.command.EtlCommand
+
+object CliMain {
+  class GlobalArgs {
+    @Parameter(names = Array("-h", "--help"))
+    var help: Boolean = false
+  }
+
+  def main(args: Array[String]): Unit = {
+    val commands = Map(EtlCommand.name -> EtlCommand)
+
+    val jCommanderBuilder = JCommander.newBuilder()
+    val globalArgs = new GlobalArgs
+    jCommanderBuilder.addObject(globalArgs)
+    for (command <- commands.values) {
+      jCommanderBuilder.addCommand(command.name, command.args)
+    }
+    val jCommander = jCommanderBuilder.build()
+
+    jCommander.parse(args:_*)
+
+    if (globalArgs.help) {
+      jCommander.usage()
+      return
+    }
+
+    val commandName = jCommander.getParsedCommand
+    if (commandName == null) {
+      jCommander.usage()
+      return
+    }
+
+    val command = commands(commandName)
+
+    command()
+  }
+}
