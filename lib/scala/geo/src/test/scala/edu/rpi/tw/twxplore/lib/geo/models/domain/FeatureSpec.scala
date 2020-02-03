f4bbc6e041ac13b31ce6995164e006a544b2571e
Minor Gordon <gordom6@rpi.edu>
2020-02-03 12:27:10 -0500

    update domain models against scena

--- lib/scala/geo/src/test/scala/edu/rpi/tw/twxplore/lib/geo/models/domain/FeatureSpec.scala
+++ lib/scala/geo/src/test/scala/edu/rpi/tw/twxplore/lib/geo/models/domain/FeatureSpec.scala
@@ -1,14 +1,16 @@
 package edu.rpi.tw.twxplore.lib.geo.models.domain
 
 import io.github.tetherlessworld.scena.Rdf
+import org.apache.jena.rdf.model.ModelFactory
 import org.scalatest.{Matchers, WordSpec}
 
 class FeatureSpec extends WordSpec with Matchers {
   "The Feature companion object" should {
     "serialize and deserialize Features" in {
       val expected = TestData.feature
-      val actualResource = Rdf.write(expected)
-      val actual = Rdf.read[Feature](actualResource)
+      val jenaModel = ModelFactory.createDefaultModel()
+      val jenaResource = Rdf.write(jenaModel, expected)
+      val actual = Rdf.read[Feature](jenaResource)
       actual should equal(expected)
     }
   }
