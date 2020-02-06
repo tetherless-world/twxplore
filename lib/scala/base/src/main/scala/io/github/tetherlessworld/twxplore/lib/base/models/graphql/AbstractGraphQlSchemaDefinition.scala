4221f615f03a65309bc326fd63d3944162d9bab8
Minor Gordon <gordom6@rpi.edu>
2020-02-06 13:44:10 -0500

    real AbstractgraphQlSchemaDefinition

---
+++ lib/scala/base/src/main/scala/io/github/tetherlessworld/twxplore/lib/base/models/graphql/AbstractGraphQlSchemaDefinition.scala
@@ -0,0 +1,17 @@
+package io.github.tetherlessworld.twxplore.lib.base.models.graphql
+
+import io.lemonlabs.uri.Uri
+import sangria.schema.{Argument, IntType, ScalarAlias, StringType}
+
+abstract class AbstractGraphQlSchemaDefinition {
+  // Scalar aliases
+  implicit val UriType = ScalarAlias[Uri, String](
+    StringType, _.toString, uri => Right(Uri.parse(uri))
+  )
+
+  // Scalar argument types
+  val LimitArgument = Argument("limit", IntType, description = "Limit")
+  val OffsetArgument = Argument("offset", IntType, description = "Offset")
+  val TextArgument = Argument("text", StringType, description = "Text")
+  val UriArgument = Argument("uri", UriType, description = "URI")
+}
