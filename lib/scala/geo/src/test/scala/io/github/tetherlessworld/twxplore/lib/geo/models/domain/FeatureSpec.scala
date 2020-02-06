aff32a77e1ad1e2bdac74b777fcbeec31f780f33
Minor Gordon <gordom6@rpi.edu>
2020-02-06 13:18:55 -0500

    move packages around

--- lib/scala/geo/src/test/scala/io/github/tetherlessworld/twxplore/lib/geo/models/domain/FeatureSpec.scala
+++ lib/scala/geo/src/test/scala/io/github/tetherlessworld/twxplore/lib/geo/models/domain/FeatureSpec.scala
@@ -1,5 +1,7 @@
 package io.github.tetherlessworld.twxplore.lib.geo.models.domain
 
+import io.github.tetherlessworld.twxplore.lib.test.models.domain.DomainModelSpec
+
 class FeatureSpec extends DomainModelSpec {
   "The Feature companion object" should {
     "serialize and deserialize Features" in {
--- lib/scala/tree/src/test/scala/io/github/tetherlessworld/twxplore/lib/tree/TreeDataCsvTransformerSpec.scala
+++ lib/scala/tree/src/test/scala/io/github/tetherlessworld/twxplore/lib/tree/TreeDataCsvTransformerSpec.scala
@@ -1,4 +1,4 @@
-import io.github.tetherlessworld.twxplore.lib.tree.TreeDataCsvTransformer
+package io.github.tetherlessworld.twxplore.lib.test
 
 class TreeDataCsvTransformerSpec extends WordSpec with Matchers {
   "TreeDataCsvTranformerSpec" can {
