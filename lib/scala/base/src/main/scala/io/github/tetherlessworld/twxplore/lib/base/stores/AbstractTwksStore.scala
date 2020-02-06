4ae25d6f98f98c9d20bff406af6a4ac43f470c90
Minor Gordon <gordom6@rpi.edu>
2020-02-06 12:10:26 -0500

    AbstractTwksStore updates

--- lib/scala/base/src/main/scala/io/github/tetherlessworld/twxplore/lib/base/stores/AbstractTwksStore.scala
+++ lib/scala/base/src/main/scala/io/github/tetherlessworld/twxplore/lib/base/stores/AbstractTwksStore.scala
@@ -1,24 +1,11 @@
 package io.github.tetherlessworld.twxplore.lib.base.stores
 
-import edu.rpi.tw.twks.client.{RestTwksClient, RestTwksClientConfiguration, TwksClient}
-import org.apache.commons.configuration2.PropertiesConfiguration
+import edu.rpi.tw.twks.client.RestTwksClient
 import org.apache.jena.query.{Query, QueryExecution}
-import play.api.{Configuration, Logging}
+import play.api.Logging
 
-abstract class AbstractTwksStore(configuration: Configuration) extends Logging {
-  protected val twksClient = newTwksClient()
-
-  private def newTwksClient(): TwksClient = {
-    val properties = new PropertiesConfiguration
-    for (entry <- configuration.entrySet) {
-      val value = entry._2.unwrapped()
-      if (value.isInstanceOf[String]) {
-        properties.setProperty(entry._1, value.asInstanceOf[String])
-      }
-    }
-
-    new RestTwksClient(RestTwksClientConfiguration.builder().set(properties).build())
-  }
+abstract class AbstractTwksStore(configuration: TwksStoreConfiguration) extends Logging {
+  protected val twksClient = new RestTwksClient(configuration.twksClientConfiguration)
 
   protected final def withAssertionsQueryExecution[T](query: Query)(f: (QueryExecution) => T): T = {
     val queryExecution = twksClient.queryAssertions(query)
