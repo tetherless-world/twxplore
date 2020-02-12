5b73a1c0505cabef9e6983bcc84cc1641012e325
KristoferKwan <kristofer.kwan@gmail.com>
2020-02-12 09:46:00 -0500

    ADD: graphql files

---
+++ app/tree/app/graphql/GraphQlSchemaDefinition.scala
@@ -0,0 +1,308 @@
+package models.graphql
+
+import java.util.Date
+
+import edu.rpi.tw.twks.uri.Uri
+import io.github.tetherlessworld.twxplore.lib.base.models.graphql.AbstractGraphQlSchemaDefinition
+import io.github.tetherlessworld.twxplore.lib.geo.models.domain._
+import play.api.libs.json
+import play.api.libs.json.{JsResult, JsString, JsSuccess, JsValue}
+import sangria.macros.derive._
+import sangria.marshalling.{CoercedScalaResultMarshaller, FromInput}
+import sangria.schema.{Argument, Field, FloatType, InputField, ListType, ScalarAlias, Schema, StringType, fields}
+
+object GraphQlSchemaDefinition extends AbstractGraphQlSchemaDefinition{
+  // Scalar Formats
+  implicit val uriFormat = new json.Format[Uri] {
+    override def reads(json: JsValue): JsResult[Uri] = JsSuccess(Uri.parse(json.asInstanceOf[JsString].value))
+    override def writes(o: Uri): JsValue = JsString(o.toString)
+  }
+
+
+  // Scalar aliases
+  implicit val ScalaFloatType = ScalarAlias[Float, Double](
+    FloatType, _.toDouble, value => {
+      Right(value.toFloat)
+    }
+  )
+
+  implicit val DateType = ScalarAlias[Date, String](
+    StringType, _.toString, date => {
+      val dateFormatter = new java.text.SimpleDateFormat("EEE MMM dd hh:mm:ss zzz yyyy")
+      Right(dateFormatter.parse(date))
+    }
+  )
+  implicit val CurbLocType = ScalarAlias[CurbLoc, String](
+    StringType, _.label, curbLoc => {
+      val result = curbLoc match {
+        case "OffsetFromCurb" => OffsetFromCurb
+        case "OnCurb" => OnCurb
+      }
+      Right(result)
+    }
+  )
+  implicit val StatusType = ScalarAlias[Status, String](
+    StringType, _.label, status => {
+      val result = status match {
+        case "Alive" => Alive
+        case "Dead" => Dead
+        case "Stump" => Stump
+        case _ => Dead
+      }
+      Right(result)
+    }
+  )
+  implicit val GuardsType = ScalarAlias[Guards, String](
+    StringType, _.label, guards => {
+      val result = guards match {
+        case "Helpful" => Helpful
+        case "Harmful" => Harmful
+        case "Unsure" => Unsure
+      }
+      Right(result)
+    }
+  )
+  implicit val HealthType = ScalarAlias[Health, String](
+    StringType, _.label, health => {
+      val result = health match {
+        case "Fair" => Fair
+        case "Good" => Good
+        case "Poor" => Poor
+      }
+      Right(result)
+    }
+  )
+  implicit val ProblemType = ScalarAlias[Problems, String](
+    StringType, _.label, problem => {
+      val result = problem match {
+        case "BranchLights" => BranchLights
+        case "BranchOther" => BranchOther
+        case "BranchShoe" => BranchShoe
+        case "MetalGrates" => MetalGrates
+        case "Stones" => Stones
+        case "TrunkLights" => TrunkLights
+        case "TrunkOther" => TrunkOther
+        case "TrunkWire" => TrunkWire
+        case "RootGrate" => RootGrate
+        case "RootLights" => RootLights
+        case "RootStone" => RootStone
+        case "Sneakers" => Sneakers
+        case "WiresRope" => WiresRope
+      }
+      Right(result)
+    }
+  )
+  implicit val SidewalkType = ScalarAlias[Sidewalk, String](
+    StringType, _.label, sidewalk => {
+      val result = sidewalk match {
+        case "NoDamage" => NoDamage
+        case "Damage" => Damage
+      }
+      Right(result)
+    }
+  )
+  implicit val StewardType = ScalarAlias[Steward, String](
+    StringType, _.label, steward => {
+      val result = steward match {
+        case "OneOrTwo" => OneOrTwo
+        case "ThreeOrFour" => ThreeOrFour
+      }
+      Right(result)
+    }
+  )
+  implicit val UserType = ScalarAlias[UserType, String](
+    StringType, _.label, user => {
+      val result = user match {
+        case "TreesCountStaff" => TreesCountStaff
+        case "NYCParksStaff" => NYCParksStaff
+        case "Volunteer" => Volunteer
+      }
+      Right(result)
+    }
+  )
+//  // Scalar argument types
+//  val LimitArgument = Argument("limit", IntType, description = "Limit")
+//  val OffsetArgument = Argument("offset", IntType, description = "Offset")
+//  val UriArgument = Argument("uri", UriType, description= "URI")
+
+  // Domain model types, in dependence order
+
+//  implicit val StateInputType = deriveInputObjectType[State](
+//    InputObjectTypeName("StateFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val CityInputType = deriveInputObjectType[City](
+//    InputObjectTypeName("CityFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val boroughInputType = deriveInputObjectType[Borough](
+//    InputObjectTypeName("BoroughFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val NtaInputType = deriveInputObjectType[Nta](
+//    InputObjectTypeName("NtaFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val BlockInputType = deriveInputObjectType[Block](
+//    InputObjectTypeName("BlockFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val CensusTractInputType = deriveInputObjectType[CensusTract](
+//    InputObjectTypeName("CensusTractFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val PostCodeInputType = deriveInputObjectType[Postcode](
+//    InputObjectTypeName("PostcodeFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val SpeciesInputType = deriveInputObjectType[TreeSpecies](
+//    InputObjectTypeName("SpeciesFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+//
+//  implicit val ZipCityInputType = deriveInputObjectType[ZipCity](
+//    InputObjectTypeName("ZipCityFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType))
+//  )
+
+  implicit val GeometryInputType = deriveInputObjectType[Geometry](
+    InputObjectTypeName("GeometryFieldsInput"),
+    ReplaceInputField("uri", InputField("uri", UriType))
+  )
+
+//  implicit val TreeInputType = deriveInputObjectType[Tree](
+//    InputObjectTypeName("TreeFieldsInput"),
+//    ReplaceInputField("uri", InputField("uri", UriType)),
+//    ReplaceInputField("createdAt", InputField("createdAt", DateType)),
+//    ReplaceInputField("curbLoc", InputField("curbLoc", CurbLocType)),
+//  )
+
+  implicit val StateType = deriveObjectType[GraphQlSchemaContext, State](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val CityType = deriveObjectType[GraphQlSchemaContext, City](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val BoroughType = deriveObjectType[GraphQlSchemaContext, Borough](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val NtaType = deriveObjectType[GraphQlSchemaContext, Nta](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val BlockType = deriveObjectType[GraphQlSchemaContext, Block](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val CensusTractType = deriveObjectType[GraphQlSchemaContext, CensusTract](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val PostCodeType = deriveObjectType[GraphQlSchemaContext, Postcode](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val SpeciesType = deriveObjectType[GraphQlSchemaContext, TreeSpecies](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val ZipCityType = deriveObjectType[GraphQlSchemaContext, ZipCity](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val GeometryType = deriveObjectType[GraphQlSchemaContext, Geometry](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val FeatureType = deriveObjectType[GraphQlSchemaContext, Feature](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri))
+  )
+
+  implicit val TreeType = deriveObjectType[GraphQlSchemaContext, Tree](
+    ReplaceField("uri", Field("uri", UriType, resolve = _.value.uri)),
+  )
+
+
+  implicit val manualGeo = new FromInput[Geometry] {
+    val marshaller = CoercedScalaResultMarshaller.default
+    def fromResult(node: marshaller.Node) = {
+      val ad = node.asInstanceOf[Map[String, Any]]
+
+      Geometry(
+        label = ad.get("label").flatMap(_.asInstanceOf[Option[String]]),
+        uri = ad("uri").asInstanceOf[Uri],
+        wkt = ad("wkt").asInstanceOf[String]
+      )
+    }
+  }
+
+  implicit val manualTree = new FromInput[Tree] {
+    val marshaller = CoercedScalaResultMarshaller.default
+    def fromResult(node: marshaller.Node) = {
+      val ad = node.asInstanceOf[Map[String, Any]]
+
+      Tree(
+        id = ad.get("id").asInstanceOf[Int],
+        createdAt = ad.get("createdAt").asInstanceOf[Date],
+        dbh = ad.get("dbh").asInstanceOf[Int],
+        stump = ad.get("stump").asInstanceOf[Int],
+        block = ad.get("block").asInstanceOf[Block],
+        curbLoc = ad.get("curbLoc").asInstanceOf[CurbLoc],
+        status = ad.get("status").asInstanceOf[Status],
+        health = ad.get("health").asInstanceOf[Option[Health]],
+        species = ad.get("species").asInstanceOf[Option[TreeSpecies]],
+        steward = ad.get("steward").asInstanceOf[Option[Steward]],
+        guards = ad.get("guards").asInstanceOf[Option[Guards]],
+        sidewalk = ad.get("sidewalk").asInstanceOf[Option[Sidewalk]],
+        userType = ad.get("userType").asInstanceOf[UserType],
+        problems = ad.get("problems").toList.map( problem => problem.asInstanceOf[Problems]),
+        address = ad.get("address").asInstanceOf[String],
+        postcode = ad.get("postcode").asInstanceOf[Postcode],
+        city = ad.get("city").asInstanceOf[City],
+        zipCity = ad.get("zipCity").asInstanceOf[ZipCity],
+        community = ad.get("community").asInstanceOf[Int],
+        borough = ad.get("borough").asInstanceOf[Borough],
+        cncldist = ad.get("cncldist").asInstanceOf[Int],
+        stateAssembly = ad.get("stateAssembly").asInstanceOf[Int],
+        stateSenate = ad.get("stateSenate").asInstanceOf[Int],
+        NTA = ad.get("NTA").asInstanceOf[Nta],
+        boroughCount = ad.get("boroughCount").asInstanceOf[Int],
+        state = ad.get("state").asInstanceOf[State],
+        latitude = ad.get("latitude").asInstanceOf[Float],
+        longitude = ad.get("longitude").asInstanceOf[Float],
+        x_sp = ad.get("x_sp").asInstanceOf[Float],
+        y_sp = ad.get("y_sp").asInstanceOf[Float],
+        censusTract = ad.get("censusTract").asInstanceOf[Option[CensusTract]],
+        bin = ad.get("bin").asInstanceOf[Option[Int]],
+        bbl = ad.get("bbl").asInstanceOf[Option[Long]],
+        uri = ad.get("uri").asInstanceOf[Uri]
+      )
+    }
+  }
+
+  // Argument typqes
+  val GeometryArgument = Argument("geometry", GeometryInputType, description="Geometry Input")
+
+  // Query types
+  val RootQueryType = sangria.schema.ObjectType("RootQuery", fields[GraphQlSchemaContext, Unit](
+    Field("trees", ListType(TreeType), arguments = LimitArgument :: OffsetArgument :: Nil, resolve = (ctx) => ctx.ctx.store.getTrees(limit = ctx.args.arg("limit"), offset = ctx.args.arg("offset"))),
+    Field("features", ListType(FeatureType), arguments = LimitArgument :: OffsetArgument ::  Nil, resolve = (ctx) => ctx.ctx.store.getFeatures(limit = ctx.args.arg("limit"), offset = ctx.args.arg("offset"))),
+    Field("featureByUri", FeatureType, arguments = UriArgument ::  Nil, resolve = (ctx) => ctx.ctx.store.getFeatureByUri(featureUri = ctx.args.arg("uri"))),
+    Field("featuresContaining", ListType(FeatureType), arguments = GeometryArgument :: Nil, resolve = (ctx) => ctx.ctx.store.getFeaturesContaining(ctx.args.arg("geometry")) )
+  ))
+
+  // Schema
+  val schema = Schema(
+    RootQueryType
+  )
+}
---
+++ app/tree/app/stores/TwksStore.scala
@@ -0,0 +1,180 @@
+package stores
+import edu.rpi.tw.twks.client.{RestTwksClient, RestTwksClientConfiguration, TwksClient}
+import edu.rpi.tw.twks.uri.Uri
+import io.github.tetherlessworld.scena.Rdf
+import io.github.tetherlessworld.twxplore.lib.base.models.domain.vocabulary._
+import io.github.tetherlessworld.twxplore.lib.geo.models.domain._
+import org.apache.jena.geosparql.implementation.vocabulary.{Geo, GeoSPARQL_URI}
+import org.apache.jena.query.{Query, QueryExecution, QueryFactory}
+import org.apache.jena.vocabulary.{RDF, RDFS}
+
+import scala.collection.JavaConverters._
+
+class TwksStore(serverBaseUrl: String) extends Store {
+  private val client: TwksClient = new RestTwksClient(RestTwksClientConfiguration.builder().setServerBaseUrl(serverBaseUrl).build())
+
+  override def getTrees(limit: Int, offset: Int): List[Tree] =
+    getTreesByUris(getTreeUris(limit = limit, offset = offset))
+
+  private def getTreesByUris(TreeUris: List[Uri]): List[Tree] = {
+    // Should be safe to inject featureUris since they've already been parsed as URIs
+    val query = QueryFactory.create(
+      s"""
+         |PREFIX rdf: <${RDF.getURI}>
+         |PREFIX treeR: <${TREE.URI + "resource"}>
+         |PREFIX treeP: <${TREE.URI + "property"}>
+         |PREFIX schema: <${Schema.URI}>
+         |CONSTRUCT {
+         |  ?tree ?treeP ?treeO .
+         |  ?tree rdf:type treeR:Tree .
+         |  ?block ?blockP ?blockO .
+         |  ?borough ?boroughP ?boroughO .
+         |  ?censusTract ?censusTractP ?censusTractO .
+         |  ?city ?cityP ?cityO .
+         |  ?nta ?ntaP ?ntaO .
+         |  ?postcode ?postcodeP ?postcodeO .
+         |  ?species ?speciesP ?speciesO .
+         |  ?state ?stateP ?stateO .
+         |  ?zipCity ?zipCityP ?zipCityO .
+         |} WHERE {
+         |  VALUES ?tree { ${TreeUris.map(TreeUri => "<" + TreeUri.toString() + ">").mkString(" ")} }
+         |  ?tree treeP:block ?block .
+         |  ?tree treeP:borough ?borough .
+         |  ?tree treeP:censusTract ?block .
+         |  ?tree schema:city ?city .
+         |  ?tree treeP:NTA ?nta .
+         |  ?tree schema:postcode ?postcode .
+         |  ?tree treeP:species ?species .
+         |  ?tree schema:state ?state .
+         |  ?tree treeP:zipCity ?zipCity .
+         |  ?block ?blockP ?blockO .
+         |  ?borough ?boroughP ?boroughO .
+         |  ?censusTract ?censusTractP ?censusTractO .
+         |  ?city ?cityP ?cityO .
+         |  ?nta ?ntaP ?ntaO .
+         |  ?postcode ?postcodeP ?postcodeO .
+         |  ?species ?speciesP ?speciesO .
+         |  ?state ?stateP ?stateO .
+         |  ?tree ?treeP ?treeO .
+         |  ?zipCity ?zipCityP ?zipCityO .
+         |}
+         |""".stripMargin)
+    withQueryExecution(query) { queryExecution =>
+      val model = queryExecution.execConstruct()
+      model.listSubjectsWithProperty(RDF.`type`, TREE.TREE_URI_PREFIX).asScala.toList.map(resource => Rdf.read[Tree](resource))
+    }
+  }
+
+  private def getTreeUris(limit: Int, offset: Int): List[Uri] = {
+    val query = QueryFactory.create(
+      s"""
+         |PREFIX rdf: <${RDF.getURI}>
+         |PREFIX tree: <${TREE.URI + "resource"}>
+         |
+         |SELECT DISTINCT ?feature WHERE {
+         |  ?feature rdf:type tree:tree .
+         |} LIMIT $limit OFFSET $offset
+         |""".stripMargin)
+    withQueryExecution(query) {
+      queryExecution =>
+        queryExecution.execSelect().asScala.toList.map(querySolution => Uri.parse(querySolution.get("tree").asResource().getURI))
+    }
+  }
+
+  override def getFeatures(limit: Int, offset: Int): List[Feature] =
+    getFeaturesByUris(getFeatureUris(limit = limit, offset = offset))
+
+  override def getFeaturesCount(): Int = {
+      val query = QueryFactory.create(
+        s"""
+           |PREFIX geo: <${GeoSPARQL_URI.GEO_URI}>
+           |PREFIX rdf: <${RDF.getURI}>
+           |PREFIX sf: <${GeoSPARQL_URI.SF_URI}>
+           |SELECT (COUNT(DISTINCT ?feature) AS ?count)
+           |WHERE {
+           |  ?feature rdf:type geo:Feature .
+           |  ?feature geo:hasDefaultGeometry ?geometry .
+           |  ?geometry rdf:type sf:Geometry .
+           |}
+           |""".stripMargin)
+      withQueryExecution(query) {
+        queryExecution =>
+          queryExecution.execSelect().next().get("count").asLiteral().getInt
+    }
+  }
+
+  override def getFeaturesContaining(geometry: Geometry): List[Feature] = {
+    val query = QueryFactory.create(
+      s"""
+         |PREFIX geo: <${GeoSPARQL_URI.GEO_URI}>
+         |PREFIX geof: <${GeoSPARQL_URI.GEOF_URI}>
+         |PREFIX rdf: <${RDF.getURI}>
+         |PREFIX rdfs: <${RDFS.getURI}>
+         |SELECT ?feature ?featureLabel ?featureGeometry ?featureGeometryLabel ?featureGeometryWkt
+         |WHERE {
+         |  ?feature geo:hasDefaultGeometry ?featureGeometry .
+         |  ?feature rdfs:label ?featureLabel .
+         |  ?featureGeometry geo:asWKT ?featureGeometryWkt .
+         |  ?featureGeometry rdfs:label ?featureGeometryLabel .
+         |  FILTER(geof:sfContains(?featureGeometryWkt, <${geometry.uri}>)) .
+         |}
+         |""".stripMargin)
+    withQueryExecution(query) {
+      queryExecution =>
+        val model = queryExecution.execConstruct()
+        model.listSubjectsWithProperty(RDF.`type`, Geo.FEATURE_RES).asScala.toList.map(resource => Rdf.read[Feature](resource))
+    }
+  }
+
+
+  def getFeatureByUri(featureUri: Uri): Feature =
+    getFeaturesByUris(List(featureUri)).head
+
+  private def getFeaturesByUris(featureUris: List[Uri]): List[Feature] = {
+    // Should be safe to inject featureUris since they've already been parsed as URIs
+    val query = QueryFactory.create(
+      s"""
+         |PREFIX geo: <${GeoSPARQL_URI.GEO_URI}>
+         |PREFIX rdf: <${RDF.getURI}>
+         |PREFIX sf: <${GeoSPARQL_URI.SF_URI}>
+         |CONSTRUCT {
+         |  ?feature ?featureP ?featureO .
+         |  ?feature rdf:type geo:Feature .
+         |  ?geometry ?geometryP ?geometryO .
+         |} WHERE {
+         |  VALUES ?feature { ${featureUris.map(featureUri => "<" + featureUri.toString() + ">").mkString(" ")} }
+         |  ?feature geo:hasDefaultGeometry ?geometry .
+         |  ?feature ?featureP ?featureO .
+         |  ?geometry ?geometryP ?geometryO .
+         |}
+         |""".stripMargin)
+    withQueryExecution(query) { queryExecution =>
+      val model = queryExecution.execConstruct()
+      model.listSubjectsWithProperty(RDF.`type`, Geo.FEATURE_RES).asScala.toList.map(resource => Rdf.read[Feature](resource))
+    }
+  }
+
+  private def getFeatureUris(limit: Int, offset: Int): List[Uri] = {
+    val query = QueryFactory.create(
+      s"""
+         |PREFIX geo: <${GeoSPARQL_URI.GEO_URI}>
+         |PREFIX rdf: <${RDF.getURI}>
+         |SELECT DISTINCT ?feature WHERE {
+         |  ?feature rdf:type geo:Feature .
+         |} LIMIT $limit OFFSET $offset
+         |""".stripMargin)
+    withQueryExecution(query) {
+      queryExecution =>
+        queryExecution.execSelect().asScala.toList.map(querySolution => Uri.parse(querySolution.get("feature").asResource().getURI))
+    }
+  }
+
+  private def withQueryExecution[T](query: Query)(f: (QueryExecution) => T): T = {
+    val queryExecution = client.queryAssertions(query)
+    try {
+      f(queryExecution)
+    } finally {
+      queryExecution.close()
+    }
+  }
+}
