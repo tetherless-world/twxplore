3aca5146de0267f323e77d0852641155c256c9c7
KristoferKwan <kristofer.kwan@gmail.com>
2020-02-24 14:46:43 -0500

    MERGE: testFix

--- lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/models/domain/SelectionGeometry.scala
+++
@@ -1,6 +0,0 @@
-package io.github.tetherlessworld.twxplore.lib.tree.models.domain
-
-import edu.rpi.tw.twks.uri.Uri
-import io.github.tetherlessworld.twxplore.lib.geo.models.domain.Geometry
-
-case class SelectionGeometry(geometry: Geometry, uri: Uri)
