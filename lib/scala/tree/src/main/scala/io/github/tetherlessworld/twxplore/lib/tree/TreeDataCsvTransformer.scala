65f315246312d33a331f13dce931a19d765c9d12
Minor Gordon <gordom6@rpi.edu>
2020-02-25 15:06:30 -0500

    add treeMeter

--- lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/TreeDataCsvTransformer.scala
+++ lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/TreeDataCsvTransformer.scala
@@ -6,11 +6,76 @@ import edu.rpi.tw.twks.uri.Uri
 import io.github.tetherlessworld.twxplore.lib.base.models.domain.vocabulary.TREE
 import io.github.tetherlessworld.twxplore.lib.geo.models.domain
 import io.github.tetherlessworld.twxplore.lib.geo.models.domain._
+import nl.grons.metrics4.scala.DefaultInstrumented
 
 import scala.collection.mutable
 import scala.io.BufferedSource
 
-case class TreeDataCsvTransformer() {
+class TreeDataCsvTransformer() extends DefaultInstrumented {
+  val uri = TREE.resourceURI
+  private val treeMeter = metrics.meter("treeMeter")
+  private var treeSpeciesMap: mutable.HashMap[String, TreeSpecies] = new mutable.HashMap()
+  private var boroughMap: mutable.HashMap[Int, Borough] = new mutable.HashMap()
+  private var ntaMap: mutable.HashMap[String, Nta] = new mutable.HashMap()
+  private var blockMap: mutable.HashMap[Int, Block] = new mutable.HashMap()
+  private var postalCode: mutable.HashMap[Int, Postcode] = new mutable.HashMap()
+  private var zipCityMap: mutable.HashMap[String, ZipCity] = new mutable.HashMap()
+  private var censusTractMap: mutable.HashMap[Int, CensusTract] = new mutable.HashMap()
+  private var city: City = City("New York City", List[Uri](), List[Uri](), Uri.parse(TREE.STATE_URI_PREFIX + ":" + "New_York"), Uri.parse(TREE.FEATURE_URI_PREFIX + ":" + "New_York"), Uri.parse(TREE.CITY_URI_PREFIX + ":" + "New York City".replace(" ", "_")))
+  private var state: State = State("New York", List[Uri](), Uri.parse(TREE.STATE_URI_PREFIX + ":" + "New York".replace(" ", "_")))
+
+  def parseCsv(filename: String, sink: TreeCsvTransformerSink): Unit = {
+    //change it back to fromResource after you're done
+    val source: BufferedSource = checkSource(filename)
+    val lineProcessor = new LineProcessor()
+    for ((line, line_no) <- source.getLines.zipWithIndex) {
+      line_no match {
+        case 0 => {}
+        case _ => {
+
+          lineProcessor.processRegions(line)
+        }
+      }
+    }
+
+    lineProcessor.generateNTAList()
+    lineProcessor.generateBoroughList()
+    lineProcessor.generateCityList()
+
+    source.close()
+    val source2: BufferedSource = checkSource(filename)
+    for ((line, line_no) <- source2.getLines.zipWithIndex) {
+      line_no match {
+        case 0 => {}
+        case _ => {
+          val problemStart: Int = line.indexOf("\"")
+          var cols: Array[String] = new Array[String](3)
+          if (problemStart != -1) {
+            val problemEnd: Int = line.indexOf("\"", problemStart + 1)
+            val new_line = replaceComma(line, problemStart, problemEnd)
+            cols = new_line.split(",", -1).map(_.trim)
+          } else {
+            cols = line.split(",", -1).map(_.trim)
+          }
+          val tree = lineProcessor.process(line)
+          sink.accept(tree)
+          treeMeter.mark()
+        }
+      }
+    }
+    sink.accept(city)
+    sink.accept(state)
+    for ((_, nta) <- ntaMap) sink.accept(nta)
+    for ((_, borough) <- boroughMap) sink.accept(borough)
+    for ((_, block) <- blockMap) sink.accept(block)
+    for ((_, code) <- postalCode) sink.accept(code)
+    for ((_, city) <- zipCityMap) sink.accept(city)
+    for ((_, species) <- treeSpeciesMap) sink.accept(species)
+    for ((_, census) <- censusTractMap) sink.accept(census)
+
+    source2.close()
+  }
+
   private def replaceComma(str: String, startIndex: Int, endIndex: Int): String = {
     str.substring(0, startIndex) + str.substring(startIndex + 1, endIndex).replace(",", "+") + str.substring(endIndex + 1)
   }
@@ -30,19 +95,84 @@ case class TreeDataCsvTransformer() {
     source
   }
 
-
-  private var treeSpeciesMap: mutable.HashMap[String, TreeSpecies] = new mutable.HashMap()
-  private var boroughMap: mutable.HashMap[Int, Borough] = new mutable.HashMap()
-  private var ntaMap: mutable.HashMap[String, Nta] = new mutable.HashMap()
-  private var blockMap: mutable.HashMap[Int, Block] = new mutable.HashMap()
-  private var postalCode: mutable.HashMap[Int, Postcode] = new mutable.HashMap()
-  private var zipCityMap: mutable.HashMap[String, ZipCity] = new mutable.HashMap()
-  private var censusTractMap: mutable.HashMap[Int, CensusTract] = new mutable.HashMap()
-  private var city: City = City("New York City", List[Uri](), List[Uri](), Uri.parse(TREE.STATE_URI_PREFIX + ":" + "New_York"), Uri.parse(TREE.FEATURE_URI_PREFIX + ":" + "New_York"), Uri.parse(TREE.CITY_URI_PREFIX + ":" + "New York City".replace(" ", "_")))
-  private var state: State = State("New York", List[Uri](), Uri.parse(TREE.STATE_URI_PREFIX + ":" + "New York".replace(" ", "_")))
-  val uri = TREE.resourceURI
-
   class LineProcessor {
+    def processRegions(line: String): Unit = {
+      val cols = convertToCols(line)
+      processPostcode(cols(25))
+      processBorough(cols(29), cols(28))
+      processNTA(cols(33), cols(34), cols(28).toInt, cols(25).toInt)
+      processBlock(cols(1), cols(33))
+    }
+
+    def generateNTAList(): Unit = {
+
+      for ((key, nta) <- ntaMap) {
+        val boroughId = nta.borough.toString.substring(nta.borough.toString.lastIndexOf(":") + 1).toInt
+        boroughMap(boroughId) = boroughMap(boroughId).addNTA(nta)
+      }
+    }
+
+    def generateBoroughList(): Unit = {
+      for ((key, borough) <- boroughMap) {
+        city = city.addBorough(borough)
+      }
+    }
+
+    def generateCityList(): Unit = state = state.addCity(city)
+
+    def process(line: String): Tree = {
+      val cols = convertToCols(line)
+
+      domain.Tree(
+        id = processTreeId(cols(0)),
+        createdAt = processCreatedAt(cols(2)),
+        dbh = processDBH(cols(3)),
+        stump = processStumpDiameter(cols(4)),
+        block = processBlock(cols(1), cols(33)).uri,
+        curbLoc = processCurbLoc(cols(5)),
+        status = processStatus(cols(6)),
+        health = processHealth(cols(7)),
+        species = {
+          val speciesObj = processTreeSpecies(cols(8), cols(9))
+          speciesObj match {
+            case None => None
+            case _ => Some(speciesObj.get.uri)
+          }
+        },
+        steward = processSteward(cols(10)),
+        guards = processGuards(cols(11)),
+        sidewalk = processSidewalk(cols(12)),
+        userType = processUserType(cols(13)),
+        problems = processProblems(cols(14)),
+        address = processAddress(cols(24)),
+        postcode = processPostcode(cols(25)).uri,
+        city = city.uri,
+        zipCity = processZipCity(cols(26)).uri,
+        community = processCommunity(cols(27)),
+        borough = processBorough(cols(29), cols(28)).uri,
+        cncldist = processCouncilDistrict(cols(30)),
+        stateAssembly = processStateAssembly(cols(31)),
+        stateSenate = processStateSenate(cols(32)),
+        NTA = processNTA(cols(33), cols(34), cols(28).toInt, cols(25).toInt).uri,
+        boroughCount = processBoroCt(cols(35)),
+        state = state.uri,
+        latitude = processLatitude(cols(37)),
+        longitude = processLongitude(cols(38)),
+        x_sp = processXStatePlane(cols(39)),
+        y_sp = processYStatePlane(cols(40)),
+        censusTract = {
+          val censusTractObj = processCensusTract(cols(42))
+          censusTractObj match {
+            case None => None
+            case _ => Some(censusTractObj.get.uri)
+          }
+        },
+        bin = processBin(cols(43)),
+        bbl = processBBL(cols(44)),
+        uri = Uri.parse(TREE.TREE_URI_PREFIX + ":" + cols(0))
+      )
+    }
+
     def processAddress(address: String): String = address
 
     def processBBL(bbl: String): Option[Long] = {
@@ -270,135 +400,5 @@ case class TreeDataCsvTransformer() {
       }
       cols
     }
-
-    def processRegions(line: String): Unit = {
-      val cols = convertToCols(line)
-      processPostcode(cols(25))
-      processBorough(cols(29), cols(28))
-      processNTA(cols(33), cols(34), cols(28).toInt, cols(25).toInt)
-      processBlock(cols(1), cols(33))
-    }
-
-    def generateNTAList(): Unit = {
-
-      for ((key, nta) <- ntaMap) {
-        val boroughId = nta.borough.toString.substring(nta.borough.toString.lastIndexOf(":") + 1).toInt
-        boroughMap(boroughId) = boroughMap(boroughId).addNTA(nta)
-      }
-    }
-
-    def generateBoroughList(): Unit = {
-      for ((key, borough) <- boroughMap) {
-        city = city.addBorough(borough)
-      }
-    }
-
-    def generateCityList(): Unit = state = state.addCity(city)
-
-
-    def process(line: String): Tree = {
-      val cols = convertToCols(line)
-
-      domain.Tree(
-        id = processTreeId(cols(0)),
-        createdAt = processCreatedAt(cols(2)),
-        dbh = processDBH(cols(3)),
-        stump = processStumpDiameter(cols(4)),
-        block = processBlock(cols(1), cols(33)).uri,
-        curbLoc = processCurbLoc(cols(5)),
-        status = processStatus(cols(6)),
-        health = processHealth(cols(7)),
-        species = {
-          val speciesObj = processTreeSpecies(cols(8), cols(9))
-          speciesObj match {
-            case None => None
-            case _ => Some(speciesObj.get.uri)
-          }
-        },
-        steward = processSteward(cols(10)),
-        guards = processGuards(cols(11)),
-        sidewalk = processSidewalk(cols(12)),
-        userType = processUserType(cols(13)),
-        problems = processProblems(cols(14)),
-        address = processAddress(cols(24)),
-        postcode = processPostcode(cols(25)).uri,
-        city = city.uri,
-        zipCity = processZipCity(cols(26)).uri,
-        community = processCommunity(cols(27)),
-        borough = processBorough(cols(29), cols(28)).uri,
-        cncldist = processCouncilDistrict(cols(30)),
-        stateAssembly = processStateAssembly(cols(31)),
-        stateSenate = processStateSenate(cols(32)),
-        NTA = processNTA(cols(33), cols(34), cols(28).toInt, cols(25).toInt).uri,
-        boroughCount = processBoroCt(cols(35)),
-        state = state.uri,
-        latitude = processLatitude(cols(37)),
-        longitude = processLongitude(cols(38)),
-        x_sp = processXStatePlane(cols(39)),
-        y_sp = processYStatePlane(cols(40)),
-        censusTract = {
-          val censusTractObj = processCensusTract(cols(42))
-          censusTractObj match {
-            case None => None
-            case _ => Some(censusTractObj.get.uri)
-          }
-        },
-        bin = processBin(cols(43)),
-        bbl = processBBL(cols(44)),
-        uri = Uri.parse(TREE.TREE_URI_PREFIX + ":" + cols(0))
-      )
-    }
-  }
-
-
-  def parseCsv(filename: String, sink: TreeCsvTransformerSink): Unit = {
-    //change it back to fromResource after you're done
-    val source: BufferedSource = checkSource(filename)
-    val lineProcessor = new LineProcessor()
-    for ((line, line_no) <- source.getLines.zipWithIndex) {
-      line_no match {
-        case 0 => {}
-        case _ => {
-
-          lineProcessor.processRegions(line)
-        }
-      }
-    }
-
-    lineProcessor.generateNTAList()
-    lineProcessor.generateBoroughList()
-    lineProcessor.generateCityList()
-
-    source.close()
-    val source2: BufferedSource = checkSource(filename)
-    for ((line, line_no) <- source2.getLines.zipWithIndex) {
-      line_no match {
-        case 0 => {}
-        case _ => {
-          val problemStart: Int = line.indexOf("\"")
-          var cols: Array[String] = new Array[String](3)
-          if (problemStart != -1) {
-            val problemEnd: Int = line.indexOf("\"", problemStart + 1)
-            val new_line = replaceComma(line, problemStart, problemEnd)
-            cols = new_line.split(",", -1).map(_.trim)
-          } else {
-            cols = line.split(",", -1).map(_.trim)
-          }
-          val tree = lineProcessor.process(line)
-          sink.accept(tree)
-        }
-      }
-    }
-    sink.accept(city)
-    sink.accept(state)
-    for ((_, nta) <- ntaMap) sink.accept(nta)
-    for ((_, borough) <- boroughMap) sink.accept(borough)
-    for ((_, block) <- blockMap) sink.accept(block)
-    for ((_, code) <- postalCode) sink.accept(code)
-    for ((_, city) <- zipCityMap) sink.accept(city)
-    for ((_, species) <- treeSpeciesMap) sink.accept(species)
-    for ((_, census) <- censusTractMap) sink.accept(census)
-
-    source2.close()
   }
 }
