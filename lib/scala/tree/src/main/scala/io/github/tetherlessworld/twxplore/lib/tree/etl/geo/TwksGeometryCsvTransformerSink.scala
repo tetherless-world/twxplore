f1623f8d5037bbac0f98d2e69cc1728b446ec0da
Minor Gordon <gordom6@rpi.edu>
2020-02-26 11:05:16 -0500

    nanopublications buffer

--- lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/geo/TwksGeometryCsvTransformerSink.scala
+++ lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/geo/TwksGeometryCsvTransformerSink.scala
@@ -1,8 +1,6 @@
 package io.github.tetherlessworld.twxplore.lib.tree.etl.geo
 
 import edu.rpi.tw.twks.api.TwksClient
-import edu.rpi.tw.twks.nanopub.Nanopublication
-import io.github.tetherlessworld.scena.{Rdf, RdfWriter}
 import io.github.tetherlessworld.twxplore.lib.geo.models.domain.Feature
 import io.github.tetherlessworld.twxplore.lib.tree.etl.AbstractTwksTransformerSink
 
@@ -11,14 +9,4 @@ final class TwksGeometryCsvTransformerSink(twksClient: TwksClient)
     with GeometryCsvTransformerSink {
 
   override def accept(feature: Feature): Unit = accept[Feature](feature)
-
-  private def accept[T](value: T)(implicit writer: RdfWriter[T]): Unit = {
-    val nanopublicationBuilder = Nanopublication.builder()
-    Rdf.write(nanopublicationBuilder.getAssertionBuilder.getModel, value)
-    val nanopublication = nanopublicationBuilder.build()
-
-    twksClient.putNanopublication(nanopublication)
-  }
-
-  override def flush(): Unit = {}
 }
--- lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/tree/TwksTreeCsvTransformerSink.scala
+++ lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/tree/TwksTreeCsvTransformerSink.scala
@@ -1,18 +1,12 @@
 package io.github.tetherlessworld.twxplore.lib.tree.etl.tree
 
 import edu.rpi.tw.twks.api.TwksClient
-import edu.rpi.tw.twks.nanopub.Nanopublication
-import io.github.tetherlessworld.scena.{Rdf, RdfWriter}
 import io.github.tetherlessworld.twxplore.lib.geo.models.domain._
 import io.github.tetherlessworld.twxplore.lib.tree.etl.AbstractTwksTransformerSink
-import nl.grons.metrics4.scala.DefaultInstrumented
-import org.apache.jena.rdf.model.ModelFactory
 
 final class TwksTreeCsvTransformerSink(twksClient: TwksClient)
   extends AbstractTwksTransformerSink(twksClient)
-    with TreeCsvTransformerSink with DefaultInstrumented {
-  private val putNanopublicationTimer = metrics.timer("putNanopublicationTimer")
-
+    with TreeCsvTransformerSink {
   override def accept(block: Block): Unit = accept[Block](block)
 
   override def accept(borough: Borough): Unit = accept[Borough](borough)
@@ -27,22 +21,9 @@ final class TwksTreeCsvTransformerSink(twksClient: TwksClient)
 
   override def accept(species: TreeSpecies): Unit = accept[TreeSpecies](species)
 
-  private def accept[T](value: T)(implicit writer: RdfWriter[T]): Unit = {
-    val model = ModelFactory.createDefaultModel()
-    Rdf.write(model, value)
-
-    val nanopublication = Nanopublication.builder().getAssertionBuilder.setModel(model).getNanopublicationBuilder.build()
-
-    putNanopublicationTimer.time {
-      twksClient.putNanopublication(nanopublication)
-    }
-  }
-
   override def accept(state: State): Unit = accept[State](state)
 
   override def accept(tree: Tree): Unit = accept[Tree](tree)
 
   override def accept(zipCity: ZipCity): Unit = accept[ZipCity](zipCity)
-
-  override def flush(): Unit = {}
 }
