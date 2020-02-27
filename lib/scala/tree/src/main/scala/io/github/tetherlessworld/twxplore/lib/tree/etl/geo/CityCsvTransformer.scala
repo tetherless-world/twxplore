9b272acae34ed6c8210c34fc0a5aa821c78e3fcd
Minor Gordon <gordom6@rpi.edu>
2020-02-27 10:40:53 -0500

    shorten code

--- lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/geo/CityCsvTransformer.scala
+++ lib/scala/tree/src/main/scala/io/github/tetherlessworld/twxplore/lib/tree/etl/geo/CityCsvTransformer.scala
@@ -3,8 +3,6 @@ package io.github.tetherlessworld.twxplore.lib.tree.etl.geo
 final class CityCsvTransformer extends GeometryCsvTransformer {
   override protected def parseCsvRow(cols: Seq[String], sink: GeometryCsvTransformerSink): Unit =
     if (cols(3) == "New York") {
-      val feature = processFeature(cols(3), cols(0), cols(3))
-      sink.accept(feature)
+      sink.accept(processFeature(cols(3), cols(0), cols(3)))
     }
-
 }
