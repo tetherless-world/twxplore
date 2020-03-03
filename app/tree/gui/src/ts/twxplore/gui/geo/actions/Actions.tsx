e5e096e5bcef5efd8bd3760cd3f84fbc095933bc
Covenant <cfaluyi@gmail.com>
2020-03-03 17:22:46 -0500

    Added functionality (state,actions,reducer) for changing mode

--- app/tree/gui/src/ts/twxplore/gui/geo/actions/Actions.tsx
+++ app/tree/gui/src/ts/twxplore/gui/geo/actions/Actions.tsx
@@ -5,6 +5,8 @@ export type APPEND_MAP = 'APPEND_MAP';
 export const APPEND_MAP: APPEND_MAP = 'APPEND_MAP';
 export type SELECT_DATA = 'SELECT_DATA'
 export const SELECT_DATA: SELECT_DATA = 'SELECT_DATA';
+export type CHANGE_MODE = 'CHANGE_MODE';
+export const CHANGE_MODE:CHANGE_MODE = 'CHANGE_MODE';
 
 export interface SelectDataAction extends Action<TreeMapQuery_TreesBySelection> {
     type: SELECT_DATA
@@ -14,7 +16,11 @@ export interface AppendMapAction extends Action<{ map: string, uri: string }> {
     type: APPEND_MAP
 }
 
-export type Action_Types = SelectDataAction | AppendMapAction
+export interface ChangeModeAction extends Action<string>{
+    type: CHANGE_MODE
+}
+
+export type Action_Types = SelectDataAction | AppendMapAction | ChangeModeAction
 
 export function sendSelectionData(data: TreeMapQuery_TreesBySelection): Action_Types {
     return {
@@ -31,5 +37,12 @@ export function sendAppendMap(a_map: string, a_uri: string): Action_Types{
             uri: a_uri
 
         }
+    }
+}
+
+export function sendChangeMode(data: string): Action_Types {
+    return {
+        type: CHANGE_MODE,
+        payload : data
     }
 }
