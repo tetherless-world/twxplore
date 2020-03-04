f827bffe2101eadc99b624a2dc189d92a1c1e615
Minor Gordon <gordom6@rpi.edu>
2020-03-04 11:05:06 -0500

    export RootState

--- app/tree/gui/src/ts/twxplore/gui/geo/reducers/RootState.ts
+++ app/tree/gui/src/ts/twxplore/gui/geo/reducers/RootState.ts
@@ -1,7 +1,7 @@
 import {RouterState} from "react-router-redux";
 import {AppState} from "twxplore/gui/geo/reducers/AppState";
 
-interface RootState {
+export interface RootState {
     keplerGl: any
     app: AppState
     routing: RouterState
