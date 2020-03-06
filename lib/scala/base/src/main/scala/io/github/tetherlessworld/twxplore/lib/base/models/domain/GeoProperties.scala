3552aee5b70c0b33ad76b0f56f28e6992d5961aa
Minor Gordon <gordom6@rpi.edu>
2020-03-06 12:09:24 -0500

    fix TreeSpec

--- lib/scala/base/src/main/scala/io/github/tetherlessworld/twxplore/lib/base/models/domain/GeoProperties.scala
+++ lib/scala/base/src/main/scala/io/github/tetherlessworld/twxplore/lib/base/models/domain/GeoProperties.scala
@@ -1,8 +1,8 @@
 package io.github.tetherlessworld.twxplore.lib.base.models.domain
 
 import edu.rpi.tw.twks.uri.Uri
-import org.apache.jena.geosparql.implementation.vocabulary.Geo
 import io.github.tetherlessworld.scena.{PropertyGetters, PropertySetters}
+import org.apache.jena.geosparql.implementation.vocabulary.Geo
 
 trait GeoProperties extends PropertyGetters with PropertySetters {
 
@@ -10,7 +10,7 @@ trait GeoProperties extends PropertyGetters with PropertySetters {
 
   final def spatialDimensionProp_=(value: Uri) = setPropertyUri(Geo.SPATIAL_DIMENSION_PROP, value.toString)
 
-  final def hasDefaultGeometry = getPropertyObjectResourceUris(Geo.HAS_DEFAULT_GEOMETRY_PROP).map(uri => Uri.parse(uri)).head
+  final def hasDefaultGeometry = getPropertyObjectResourceUris(Geo.HAS_DEFAULT_GEOMETRY_PROP).map(uri => Uri.parse(uri)).headOption
 
   final def hasDefaultGeometry_=(value: Uri) = setPropertyUri(Geo.HAS_DEFAULT_GEOMETRY_PROP, value.toString)
 }
