a78b7279e499cea6ea89ef0731689184cd4f26b3
Minor Gordon <gordom6@rpi.edu>
2020-03-06 14:23:12 -0500

    consolidate all the tree app scripts

--- app/tree/script/load-test-tree-data.sh
+++ app/tree/script/load-test-tree-data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-cd ..
+cd "$(dirname "$0")/../../.."
 ROOT_DIR=`realpath $PWD`
 cd lib/scala/tree/src/test/resources
 java -jar $ROOT_DIR/cli/tree/dist/tree-cli.jar etl
