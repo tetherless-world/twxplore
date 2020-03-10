4c791dc01907f66509d5e585f2f07ba8141ba22a
Covenant <cfaluyi@gmail.com>
2020-03-10 12:44:25 -0400

    Deleted .ts files that were in the wrong location

--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/root/initialRootState.ts
+++
@@ -1,8 +0,0 @@
-import {RootState} from "./node_modules/twxplore/gui/tree/states/root/RootState";
-import {initialAppState} from "./node_modules/twxplore/gui/tree/states/app/initialAppState";
-
-export const initialRootState: RootState = {
-  keplerGl: {},
-  app: initialAppState,
-  routing: {location: null},
-};
