fa85ba461ea855b9af2aed24334369661f6cbca1
Covenant <cfaluyi@gmail.com>
2020-03-10 12:57:06 -0400

    Routed Map Component and modified Map to add features to the TreeMap

--- app/geo/gui/src/ts/twxplore/gui/geo/main.tsx
+++ app/geo/gui/src/ts/twxplore/gui/geo/main.tsx
@@ -4,6 +4,7 @@ import {apolloClient} from "twxplore/gui/geo/api/apolloClient";
 import {createBrowserHistory} from 'history';
 import {NoRoute} from 'twxplore/gui/geo/components/error/NoRoute';
 import {Home} from 'twxplore/gui/geo/components/home/Home';
+import {Map} from 'twxplore/gui/geo/components/map/Map'
 import {Hrefs} from 'twxplore/gui/geo/Hrefs';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -11,6 +12,8 @@ import {ApolloProvider} from "react-apollo";
 import {ApolloProvider as ApolloHooksProvider} from "@apollo/react-hooks";
 import {Route, Router, Switch} from 'react-router';
 import {ConsoleLogger, LoggerContext} from '@tetherless-world/twxplore-base-lib';
+import {Provider} from "react-redux";
+import store from "./store";
 
 // Logger
 const logger = new ConsoleLogger();
@@ -21,14 +24,17 @@ const browserHistory = createBrowserHistory();
 ReactDOM.render(
     <ApolloProvider client={apolloClient}>
       <ApolloHooksProvider client={apolloClient}>
-        <LoggerContext.Provider value={logger}>
-          <Router history={browserHistory}>
-              <Switch>
-                  <Route exact path={Hrefs.home} component={Home}/>
-                  <Route component={NoRoute}/>
-              </Switch>
-          </Router>
-        </LoggerContext.Provider>
+        <Provider store={store}>
+          <LoggerContext.Provider value={logger}>
+            <Router history={browserHistory}>
+                <Switch>
+                    <Route exact path={Hrefs.home} component={Home}/>
+                    <Route exact path={Hrefs.map} component={Map} />
+                    <Route component={NoRoute}/>
+                </Switch>
+            </Router>
+          </LoggerContext.Provider>
+        </Provider>
       </ApolloHooksProvider>
     </ApolloProvider>,
     document.getElementById('root')
--- app/geo/gui/src/ts/twxplore/gui/geo/store.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/store.ts
@@ -21,11 +21,11 @@
 import {applyMiddleware, createStore, compose} from "redux";
 
 //import window from 'global/window';
-import {taskMiddleware} from "react-palm/tasks";
 import {routerMiddleware} from "react-router-redux";
+import {taskMiddleware} from "react-palm/tasks";
 import createHistory from "history/createBrowserHistory";
-import {rootReducer} from "twxplore/gui/tree/reducers/root/rootReducer";
-import {initialRootState} from "twxplore/gui/tree/states/root/initialRootState";
+import { rootReducer } from "./reducers/root/rootReducer";
+import { initialRootState } from "./states/root/initialRootState";
 //import {ADD_MAP_FEATURES} from "twxplore/gui/tree/actions/map/AddMapFeaturesAction";
 
 const history = createHistory();
@@ -44,7 +44,7 @@ export const middlewares = [taskMiddleware, routerMiddleware(history)];
     actionsWhiteList: [ADD_MAP_FEATURES]
   }) || compose;
 */
-export default createStore<any, any, any, any>(
+export default createStore<any, any, any,any>(
   rootReducer,
   initialRootState,
   compose(applyMiddleware(...middlewares))
