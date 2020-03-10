8dcbee9ca2341203603896d8c22e57dd1057a667
Covenant <cfaluyi@gmail.com>
2020-03-10 12:44:06 -0400

    Added .d.ts files with declare statements

--- app/geo/gui/types/graphql.d.ts
+++ app/geo/gui/types/graphql.d.ts
@@ -4,3 +4,13 @@ declare module "*.graphql" {
 
   export = Schema;
 }
+
+declare module 'kepler.gl'
+
+declare module 'kepler.gl/*'
+
+declare module 'global/window'
+
+declare module 'react-palm/*'
+
+
