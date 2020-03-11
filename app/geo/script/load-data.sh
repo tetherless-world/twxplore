8094a15d0dc082b49f9629bf7986d0a124239d53
Minor Gordon <gordom6@rpi.edu>
2020-03-11 17:58:39 -0400

    UlsEntity FeatureType

--- app/geo/script/load-data.sh
+++ app/geo/script/load-data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")/../../.."
-java -jar ../twks/java/dist/twks-cli-current.jar put-nanopublications data/geo/features.ttl.bz2
+java -jar ../twks/java/dist/twks-cli-current.jar put-nanopublications \
+  data/geo/loaded/tiger_line/features.ttl.bz2 \
+  data/geo/loaded/reverse_beacon/features.ttl.bz2
