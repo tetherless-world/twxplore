c1557abd2e6aef5b3471a7ea45f8c99834641113
Minor Gordon <gordom6@rpi.edu>
2020-03-16 11:26:17 -0400

    use DX instead of DE from Reverse Beacon data

--- cli/geo/geo_cli/etl/reverse_beacon/reverse_beacon_transformer.py
+++ cli/geo/geo_cli/etl/reverse_beacon/reverse_beacon_transformer.py
@@ -24,7 +24,9 @@ class ReverseBeaconTransformer(_Transformer):
     def transform(self, **kwds) -> Generator[Feature, None, None]:
         extracted_data_dir_path = DATA_DIR_PATH / "extracted" / "reverse_beacon"
         duplicate_call_sign_count = 0
+        geocode_failure_count = 0
         missing_uls_entity_count = 0
+        skipped_uls_entity_count = 0
         yielded_feature_count = 0
         for file_name in sorted(os.listdir(extracted_data_dir_path)):
             if not file_name.endswith(".zip"):
@@ -39,19 +41,31 @@ class ReverseBeaconTransformer(_Transformer):
                 with zip_file.open(file_base_name + ".csv") as csv_file:
                     csv_reader = csv.DictReader(TextIOWrapper(csv_file, "utf-8"))
                     for row_i, row in enumerate(csv_reader):
-                        if row["de_cont"] != "NA":
+                        if row_i > 0 and row_i % 10000 == 0:
+                            self.__logger.info("processed %d rows from %s", row_i, zip_file_path)
+                        # DX is the spotted station
+                        # DE is the spotting station
+                        if row["dx_cont"] != "NA":
+                            # Exclude everything outside North America
                             continue
-                        call_sign = row["callsign"]
-                        if call_sign in yielded_call_signs:
+                        dx_call_sign = row["dx"]
+                        if dx_call_sign in yielded_call_signs:
                             duplicate_call_sign_count += 1
                             continue
                         try:
-                            uls_entity = UlsEntity(**self.__uls_entities_by_call_sign[call_sign])
+                            uls_entity = UlsEntity(**self.__uls_entities_by_call_sign[dx_call_sign])
                         except KeyError:
                             missing_uls_entity_count += 1
                             continue
+                        if uls_entity.state != "NY":
+                            skipped_uls_entity_count += 1
+                            continue
                         address = f"{uls_entity.street_address}, {uls_entity.city}, {uls_entity.state} {uls_entity.zip_code}"
-                        wkt = self.__geocoder.geocode(address)
+                        try:
+                            wkt = self.__geocoder.geocode(address)
+                        except LookupError:
+                            geocode_failure_count += 1
+                            continue
                         geometry = \
                             Geometry(
                                 uri=TWXPLORE_GEO_APP_GEOMETRY[f"uls-{uls_entity.unique_system_identifier}"],
@@ -67,7 +81,8 @@ class ReverseBeaconTransformer(_Transformer):
                                 uri=TWXPLORE_GEO_APP_FEATURE[f"reverse-beacon-{file_base_name}-{row_i}"]
                             )
                         yield feature
-                        yielded_call_signs.add(call_sign)
+                        yielded_call_signs.add(dx_call_sign)
                         yielded_feature_count += 1
+
             self.__logger.info("transformed file %s", zip_file_path)
-            self.__logger.info("duplicate call signs: %d, missing ULS entities: %d, yielded features: %d", duplicate_call_sign_count, missing_uls_entity_count, yielded_feature_count)
+            self.__logger.info("duplicate call signs: %d, missing ULS entities: %d, skipped ULS entities: %d, geocode failures: %d, yielded features: %d", duplicate_call_sign_count, missing_uls_entity_count, skipped_uls_entity_count, geocode_failure_count, yielded_feature_count)
--- cli/geo/geo_cli/geocoder.py
+++ cli/geo/geo_cli/geocoder.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 import googlemaps
@@ -11,8 +12,22 @@ class Geocoder:
     def __init__(self):
         self.__cache_dir_path = DATA_DIR_PATH / "geocode_cache"
         self.__google_maps_api_key = os.environ["GOOGLE_MAPS_API_KEY"]
+        self.__logger = logging.getLogger(self.__class__.__name__)
+
+        cached_file_count = 0
+        for cache_file_name in os.listdir(self.__cache_dir_path):
+            if not cache_file_name.endswith(".json"):
+                continue
+            cache_file_base_name = os.path.splitext(cache_file_name)[0]
+            if cache_file_base_name.upper() != cache_file_base_name:
+                upper_cache_file_name = cache_file_base_name.upper() + ".json"
+                os.rename(os.path.join(self.__cache_dir_path, cache_file_name), os.path.join(self.__cache_dir_path, upper_cache_file_name))
+                self.__logger.info("renamed %s to %s", cache_file_name, upper_cache_file_name)
+            cached_file_count += 1
+        self.__logger.info("cached geocodes: %d", cached_file_count)
 
     def geocode(self, address: str) -> str:
+        address = address.upper()
         cache_file_name = sanitize_filename(address) + ".json"
         cache_file_path = self.__cache_dir_path / cache_file_name
         if os.path.isfile(cache_file_path):
@@ -21,6 +36,10 @@ class Geocoder:
         else:
             client = googlemaps.Client(key=self.__google_maps_api_key)
             geocode_result = client.geocode(address)
+            if not geocode_result:
+                self.__logger.warning("geocode of %s failed", address)
+                raise LookupError
+            self.__logger.info("geocoded %s", address)
             if not os.path.isdir(self.__cache_dir_path):
                 os.makedirs(self.__cache_dir_path)
             with open(cache_file_path, "w+") as cache_file:
