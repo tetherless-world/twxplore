1541195cd71f13cc6852af3706fe2ae87b5b146a
Covenant <cfaluyi@gmail.com>
2020-03-19 12:37:44 -0400

    Implemented store sanitizer, action Sanitizer and blacklist

--- app/geo/gui/src/ts/twxplore/gui/geo/store.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, createStore, compose} from "redux";
+import {applyMiddleware, createStore} from "redux";
 
 //import window from 'global/window';
 import {routerMiddleware} from "react-router-redux";
@@ -6,7 +6,27 @@ import {taskMiddleware} from "react-palm/tasks";
 import createHistory from "history/createBrowserHistory";
 import {rootReducer} from "./reducers/root/rootReducer";
 import {initialRootState} from "./states/root/initialRootState";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {
+  ADD_MAP_FEATURES,
+  AddMapFeaturesAction,
+} from "./actions/map/AddMapFeaturesAction";
+import {BaseAction} from "redux-actions";
+import {RootState} from "./states/root/RootState";
+
 //import {ADD_MAP_FEATURES} from "twxplore/gui/tree/actions/map/AddMapFeaturesAction";
+
+const actionSanitizer = (action: BaseAction) =>
+  action.type === ADD_MAP_FEATURES && (action as AddMapFeaturesAction).payload
+    ? {...action, payload: {}}
+    : action;
+
+const composeEnhancers = (composeWithDevTools as any)({
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  actionsBlacklist: ["@@kepler", ADD_MAP_FEATURES, ADD_MAP_FEATURES],
+  actionSanitizer,
+  stateSanitizer: (state: RootState) => (state.app ? {...state} : state),
+});
 
 const history = createHistory();
 
@@ -17,9 +37,6 @@ export const middlewares = [taskMiddleware, routerMiddleware(history)];
 //const initialState = {};
 
 // add redux devtools
-
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore<any, any, any, any>(
   rootReducer,
