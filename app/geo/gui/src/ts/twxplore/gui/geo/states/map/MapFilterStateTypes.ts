b89e0713c23f10650156327d8fa173cba245f2a7
Covenant <cfaluyi@gmail.com>
2020-03-30 15:23:37 -0400

    CamelCaase FilterStateTypes

--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFilterStateTypes.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFilterStateTypes.ts
@@ -2,7 +2,7 @@
 Each attribute of a Feature with type 'number' should have these
 properties on filterStateOfType
 */
-export interface filterStateTypes {
+export interface FilterStateTypes {
   min: number | null;
   max: number | null;
   idx: number | null;
