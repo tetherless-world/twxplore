23a0fac91d30ab7bcc197d40edb9e0129d2126f8
Covenant <cfaluyi@gmail.com>
2020-03-30 15:21:03 -0400

    implemented logger which is noplogger on prodcution

--- app/geo/gui/src/ts/twxplore/gui/geo/components/selectionPanel/SelectionPanel.tsx
+++ app/geo/gui/src/ts/twxplore/gui/geo/components/selectionPanel/SelectionPanel.tsx
@@ -14,6 +14,8 @@ import {useSelector, useDispatch, connect} from "react-redux";
 import {MapState} from "../../states/map/MapState";
 import {RootState} from "../../states/root/RootState";
 import {changeTypeVisibility} from "../../actions/map/ChangeTypeVisibilityAction";
+import {LoggerContext, ConsoleLogger} from "@tetherless-world/twxplore-base";
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -39,6 +41,8 @@ const SelectionPanelImpl: React.FunctionComponent = () => {
     dispatch(changeTypeVisibility(typeEnum));
   };
 
+  const logger : ConsoleLogger = React.useContext(LoggerContext)
+  logger.info("test")
   //const featureTypes: {[index: string]: String} = {}
 
   //const error = [gilad, jason, antoine].filter(v => v).length !== 2;
--- lib/ts/base/src/ts/twxplore/lib/base/util/logging/LoggerContext.ts
+++ lib/ts/base/src/ts/twxplore/lib/base/util/logging/LoggerContext.ts
@@ -1,5 +1,9 @@
 import {ConsoleLogger} from "./ConsoleLogger";
 import {Logger} from "./Logger";
 import * as React from "react";
+import {Environment} from "../../Environment";
+import {NopLogger} from "./NopLogger";
 
-export const LoggerContext = React.createContext<Logger>(new ConsoleLogger());
+export const LoggerContext = React.createContext<Logger>(
+  Environment.development ? new ConsoleLogger() : new NopLogger()
+);
