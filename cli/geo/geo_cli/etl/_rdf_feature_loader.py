baebdd036d75fc1c820b438a41f848cebd0909a0
Minor Gordon <gordom6@rpi.edu>
2020-04-15 14:31:37 -0400

    yield frequency and timestamp range, serialize to RDF

--- cli/geo/geo_cli/etl/_rdf_feature_loader.py
+++ cli/geo/geo_cli/etl/_rdf_feature_loader.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, RDF, Literal, XSD, RDFS
+from rdflib import Graph, RDF, Literal, XSD, RDFS, BNode
 
 from geo_cli.etl._feature_loader import _FeatureLoader
 from geo_cli.namespace import TWXPLORE_GEO_APP_ONTOLOGY, GEO, SF, SCHEMA, TWXPLORE_GEO_APP_FEATURE, \
@@ -17,40 +17,51 @@ class _RdfFeatureLoader(_FeatureLoader):
         return self
 
     def _add_feature_to_graph(self, feature):
-        self._graph.add((feature.uri, RDF.type, GEO.Feature))
+        feature_resource = self._graph.resource(feature.uri)
+
+        feature_resource.add(RDF.type, GEO.Feature)
 
         if feature.frequency is not None:
-            self._graph.add((feature.uri, TWXPLORE_GEO_APP_ONTOLOGY.frequency, Literal(feature.frequency, datatype=XSD.float)))
+            feature_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.frequency, Literal(feature.frequency, datatype=XSD.float))
         elif feature.frequency_range is not None:
-            raise NotImplementedError
+            frequency_range_resource = self._graph.resource(BNode())
+            frequency_range_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.frequencyMaximum, Literal(feature.frequency_range[1], datatype=XSD.float))
+            frequency_range_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.frequencyMinimum, Literal(feature.frequency_range[0], datatype=XSD.float))
+            feature_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.frequencyRange, frequency_range_resource)
 
-        self._graph.add((feature.uri, GEO.hasDefaultGeometry, feature.geometry.uri))
-        self.__add_geometry_to_graph(feature.geometry)
+        feature_resource.add(GEO.hasDefaultGeometry, self.__add_geometry_to_graph(feature.geometry))
 
         if feature.label is not None:
-            self._graph.add((feature.uri, RDFS.label, Literal(feature.label)))
+            feature_resource.add(RDFS.label, Literal(feature.label))
 
         if feature.locality is not None:
-            self._graph.add((feature.uri, SCHEMA.addressLocality, Literal(feature.locality)))
+            feature_resource.add(SCHEMA.addressLocality, Literal(feature.locality))
 
         if feature.postal_code is not None:
-            self._graph.add((feature.uri, SCHEMA.postalCode, Literal(feature.postal_code)))
+            feature_resource.add(SCHEMA.postalCode, Literal(feature.postal_code))
 
         if feature.regions is not None:
             for region in feature.regions:
-                self._graph.add((feature.uri, SCHEMA.addressRegion, Literal(region)))
+                feature_resource.add(SCHEMA.addressRegion, Literal(region))
 
         if feature.timestamp is not None:
-            self._graph.add((feature.uri, TWXPLORE_GEO_APP_ONTOLOGY.timestamp, Literal(feature.timestamp, datatype=XSD.dateTime)))
+            feature_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.timestamp, Literal(feature.timestamp, datatype=XSD.dateTime))
+        elif feature.timestamp_range is not None:
+            timestamp_range_resource = self._graph.resource(BNode())
+            timestamp_range_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.timestampMaximum, Literal(feature.timestamp_range[1], datatype=XSD.dateTime))
+            timestamp_range_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.timestampMinimum, Literal(feature.timestamp_range[0], datatype=XSD.dateTime))
+            feature_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.timestampRange, timestamp_range_resource)
 
         if feature.transmission_power is not None:
-            self._graph.add((feature.uri, TWXPLORE_GEO_APP_ONTOLOGY.transmissionPower, Literal(feature.transmission_power, datatype=XSD.int)))
+            feature_resource.add(TWXPLORE_GEO_APP_ONTOLOGY.transmissionPower, Literal(feature.transmission_power, datatype=XSD.int))
 
         if feature.type is not None:
-            self._graph.add((feature.uri, RDF.type, feature.type))
+            feature_resource.add(RDF.type, feature.type)
 
     def __add_geometry_to_graph(self, geometry):
-        self._graph.add((geometry.uri, RDF.type, SF.Geometry))
+        geometry_resource = self._graph.resource(geometry.uri)
+        geometry_resource.add(RDF.type, SF.Geometry)
         if geometry.label is not None:
-            self._graph.add((geometry.uri, RDFS.label, Literal(geometry.label)))
-        self._graph.add((geometry.uri, GEO.asWKT, Literal(geometry.wkt, datatype=GEO.wktLiteral)))
+            geometry_resource.add(RDFS.label, Literal(geometry.label))
+        geometry_resource.add(GEO.asWKT, Literal(geometry.wkt, datatype=GEO.wktLiteral))
+        return geometry_resource
--- cli/geo/geo_cli/etl/dsa/dsa_request_feature_transformer.py
+++ cli/geo/geo_cli/etl/dsa/dsa_request_feature_transformer.py
@@ -1,11 +1,11 @@
 import json
+from datetime import datetime
 from typing import Generator
 
 from rdflib import URIRef
 
 from geo_cli.etl._feature_transformer import _FeatureTransformer
 from geo_cli.model.feature import Feature
-from geo_cli.model.frequency_range import FrequencyRange
 from geo_cli.model.geometry import Geometry
 from geo_cli.namespace import TWXPLORE_GEO_APP_ONTOLOGY
 from geo_cli.path import ROOT_DIR_PATH
@@ -13,6 +13,12 @@ from geo_cli.path import ROOT_DIR_PATH
 
 class DsaRequestFeatureTransformer(_FeatureTransformer):
     def transform(self, **kwds) -> Generator[Feature, None, None]:
+        def parse_datetime(value: str) -> datetime:
+            assert value.endswith("Z[GMT]"), value
+            value = value[:-len("Z[GMT]")]
+            value += "+00:00"
+            return datetime.fromisoformat(value)
+
         requests_json_file_path = (ROOT_DIR_PATH / ".." / "DynamicSpectrumAccess" / "ontologies" / "requests.json").absolute()
         with open(requests_json_file_path) as requests_json_file:
             request_json_objects = json.load(requests_json_file)
@@ -20,15 +26,19 @@ class DsaRequestFeatureTransformer(_FeatureTransformer):
                 location_json_object = request_json_object["location"]
                 yield \
                     Feature(
-                        frequency_range=FrequencyRange(
-                            maxInclusive=request_json_object["frequencyRange"]["maximum"],
-                            minInclusive=request_json_object["frequencyRange"]["minimum"]
+                        frequency_range=(
+                            request_json_object["frequencyRange"]["minimum"],
+                            request_json_object["frequencyRange"]["maximum"]
                         ),
                         geometry=Geometry(
                             uri=URIRef(location_json_object["geometry"]["id"]),
                             wkt=location_json_object["geometry"]["wkt"]
                         ),
                         label=request_json_object["id"],
+                        timestamp_range=(
+                            parse_datetime(request_json_object["dateRange"]["from"]),
+                            parse_datetime(request_json_object["dateRange"]["until"])
+                        ),
                         type=TWXPLORE_GEO_APP_ONTOLOGY.Transmission,
                         uri=URIRef("http://purl.org/twc/dsa/request/" + request_json_object["id"])
                     )
