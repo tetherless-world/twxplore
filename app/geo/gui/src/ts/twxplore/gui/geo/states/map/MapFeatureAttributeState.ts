e5b02e3eae44a7fabd59cd22e03ce4df09a2cb0e
Covenant <cfaluyi@gmail.com>
2020-04-20 15:11:01 -0400

    filterStateOfTypes renamed to MapFeatureAttributeState with documentation added

---
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFeatureAttributeState.ts
@@ -0,0 +1,13 @@
+/*
+Originally named a filterStateOfType. Renamed MapFeatureAttributeState because it represents
+the state (min, max, filterIndex) of an attribute for features of a FeatureType. 
+
+For example, can be used to track the min/max/filterIndex of the 'frequency' attribute for features of FeatureType 'Transmission'.
+This is important info that will be used for setting filter values.
+
+*/
+export interface MapFeatureAttributeState {
+  min: number | null;
+  max: number | null;
+  filterIndex: number | null;
+}
--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFilterStateTypes.ts
+++
@@ -1,9 +0,0 @@
-/*
-Each attribute of a Feature with type 'number' should have these
-properties on filterStateOfType
-*/
-export interface FilterStateTypes {
-  min: number | null;
-  max: number | null;
-  filterIndex: number | null;
-}
