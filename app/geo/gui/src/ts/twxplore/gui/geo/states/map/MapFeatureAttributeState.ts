17f547aa2c70363be8c18257711901c810e2eed2
Covenant <cfaluyi@gmail.com>
2020-04-20 15:21:20 -0400

    Expressed uncertainties in MapFeatureAttributeState

--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFeatureAttributeState.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/MapFeatureAttributeState.ts
@@ -5,6 +5,8 @@ the state (min, max, filterIndex) of an attribute for features of a FeatureType.
 For example, can be used to track the min/max/filterIndex of the 'frequency' attribute for features of FeatureType 'Transmission'.
 This is important info that will be used for setting filter values.
 
+Unsure, if I should call this FeatureTypeAttributeState instead.
+Or maybe 'AttributeOfFeatureTypeState'
 */
 export interface MapFeatureAttributeState {
   min: number | null;
