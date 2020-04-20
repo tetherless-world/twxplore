00e4bf7d76a08a97d6cbbc88ac1ebd30f42651f5
Covenant <cfaluyi@gmail.com>
2020-04-20 15:12:28 -0400

    Fixed misnamed import

--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/FeaturesByType.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/FeaturesByType.ts
@@ -1,10 +1,10 @@
 import {MapFeature} from "./MapFeature";
 import {MapFeatureTypeState} from "./MapFeatureTypeState";
-import {FilterStateTypes} from "./MapFilterStateTypes";
+import {MapFeatureAttributeState} from "./MapFeatureAttributeState";
 
 export interface FeaturesByType {
   features: MapFeature[];
   dirty: boolean;
   featureTypeState: MapFeatureTypeState;
-  attributesState: {[attribute: string]: FilterStateTypes};
+  attributesState: {[attribute: string]: MapFeatureAttributeState};
 }
