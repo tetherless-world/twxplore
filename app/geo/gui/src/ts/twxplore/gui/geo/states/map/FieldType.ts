7775bd186b416e12813e78d40ae47fc4d882e4a6
Covenant <cfaluyi@gmail.com>
2020-04-27 10:54:34 -0400

    Recreated FieldType enum

---
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/FieldType.ts
@@ -0,0 +1,10 @@
+export enum FieldType {
+  BOOLEAN = "boolean",
+  DATE = "date",
+  GEOJSON = "geojson",
+  INTEGER = "integer",
+  REAL = "real",
+  STRING = "string",
+  TIMESTAMP = "timestamp",
+  POINT = "point",
+}
