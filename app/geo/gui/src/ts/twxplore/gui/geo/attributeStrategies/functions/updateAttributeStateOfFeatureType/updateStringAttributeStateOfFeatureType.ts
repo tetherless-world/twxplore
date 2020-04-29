e78d5eb221916cc681b0c3a8e925cda508ba3ce7
Covenant <cfaluyi@gmail.com>
2020-04-29 15:54:06 -0400

    Made clarifying comments and variable names in updateStringAttributeState

--- app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/functions/updateAttributeStateOfFeatureType/updateStringAttributeStateOfFeatureType.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/functions/updateAttributeStateOfFeatureType/updateStringAttributeStateOfFeatureType.ts
@@ -11,33 +11,29 @@ export const updateStringAttributeStateOfFeatureType = (
   attributeName: string
 ) => {
   const attributeKey = attributeName as keyof MapFeature;
-  //Specifying the exact attribute state to be updated.
-  let attributeStateOfFeatureType = attributeStatesOfFeatureType[
+  //Specifying the AttributeState to be updated based on the attribute of addedFeature that is being compared/analyzed
+  let attributeStateOfAttributeOfFeatureType = attributeStatesOfFeatureType[
     attributeName
   ] as MapStringFeatureAttributeState;
   //If this is the first time coming across this attribute for this feature type
-  if (
-    !attributeStateOfFeatureType
-    //addedFeature[attributeKey] !== null &&
-    //!attributeStateOfFeatureType.values.includes(
-    //addedFeature[attributeKey] as string
-  ) {
+  if (!attributeStateOfAttributeOfFeatureType) {
+    //Give the attribute a default MapStringFeatureAttributeState
     attributeStatesOfFeatureType[attributeName] = {
       values: [],
       filterIndex: null,
     };
-    attributeStateOfFeatureType = attributeStatesOfFeatureType[
+    attributeStateOfAttributeOfFeatureType = attributeStatesOfFeatureType[
       attributeName
     ] as MapStringFeatureAttributeState;
   }
-  //add the value of the attribute into the values list of the attributeState
+  //add the value of the attribute of the addedFeature into the values list of the attributeState if the value is not null and is not already included.
   if (
     addedFeature[attributeKey] !== null &&
-    !attributeStateOfFeatureType.values.includes(
+    !attributeStateOfAttributeOfFeatureType.values.includes(
       addedFeature[attributeKey] as string
     )
   )
-    attributeStateOfFeatureType.values.push(
+    attributeStateOfAttributeOfFeatureType.values.push(
       addedFeature[attributeKey] as string
     );
 
