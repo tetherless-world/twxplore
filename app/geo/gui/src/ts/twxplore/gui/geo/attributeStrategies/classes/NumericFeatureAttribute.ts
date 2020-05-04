289b850d39253b37ba262c9dc1564da08b1a08d6
Covenant <cfaluyi@gmail.com>
2020-05-04 09:59:46 -0400

    Removed unecessary helper functions in StringFeatureAttribute and NumericFeatureAttribute

--- app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/classes/NumericFeatureAttribute.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/classes/NumericFeatureAttribute.ts
@@ -1,6 +1,5 @@
 import {FeatureAttribute} from "./FeatureAttribute";
 import {MapFeatureAttributeState} from "../../states/map/MapFeatureAttributeState/MapFeatureAttributeState";
-import {setInitialFiltersNumeric} from "../functions/setInitialFilters/setInitialFiltersNumeric";
 import {FeatureAttributeName} from "../../states/map/FeatureAttributeName";
 import {TypeOfFeatureAttribute} from "../../states/map/TypeOfFeatureAttribute";
 import {KeplerFilterType} from "../../states/map/KeplerFilterType";
@@ -8,7 +7,8 @@ import {KeplerFieldType} from "../../states/map/KeplerFieldType";
 import {Dispatch} from "redux";
 import {buildInitialFeatureAttributeStateNumeric} from "../functions/buildInitialFeatureAtttributeState/buildInitialFeatureAttributeStateNumeric";
 import {MapFeature} from "../../states/map/MapFeature";
-import {updateNumericAttributeStateOfFeatureType} from "../functions/updateAttributeStateOfFeatureType/updateNumericAttributeStateOfFeatureType";
+import {MapNumericFeatureAttributeState} from "../../states/map/MapFeatureAttributeState/MapNumericFeatureAttributeState";
+import {setFilter} from "kepler.gl/actions";
 
 export abstract class NumericFeatureAttribute implements FeatureAttribute {
   updateAttributeStatesOfFeatureType(
@@ -17,32 +17,63 @@ export abstract class NumericFeatureAttribute implements FeatureAttribute {
     },
     addedFeature: MapFeature
   ): void {
-    updateNumericAttributeStateOfFeatureType(
-      attributeStatesOfFeatureType,
-      addedFeature,
-      this.name
-    );
+    const attributeName = this.name;
+    const attributeKey = attributeName as keyof MapFeature;
+    //Specifying the attribute state of the addedFeature's FeatureType to be updated (e.g. the attribute state of 'frequency' of Transmissions)
+    let attributeStateOfAttributeOffFeatureType = attributeStatesOfFeatureType[
+      attributeName
+    ] as MapNumericFeatureAttributeState;
+    //If this is the first time coming across this attribute for the addedFeature's FeatureType
+    if (!attributeStateOfAttributeOffFeatureType) {
+      //Give the MapNumericAttributeState a temporary default value
+      attributeStatesOfFeatureType[attributeName] = {
+        range: {
+          min: addedFeature[attributeKey] as number,
+          max: addedFeature[attributeKey] as number,
+        },
+        filterIndex: null,
+      };
+
+      return;
+    }
+    //If this is NOT the first time coming across this attribute for the addedFeature's FeatureType
+    //Compare attribute value of addedFeature to the min found in the attribute state. Set new min if necessary.
+    if (
+      (addedFeature[attributeKey] as number) <
+      attributeStateOfAttributeOffFeatureType.range!.min
+    )
+      attributeStateOfAttributeOffFeatureType.range!.min = addedFeature[
+        attributeKey
+      ] as number;
+    //Compare attribute value to the max found in the attribute state. Set new max if necessary.
+    else if (
+      (addedFeature[attributeKey] as number) >
+      attributeStateOfAttributeOffFeatureType.range!.max
+    )
+      attributeStateOfAttributeOffFeatureType.range!.max = addedFeature[
+        attributeKey
+      ] as number;
+    return;
   }
-  buildInitialFeatureAttributeState(attributeStatesOfFeatureType: {
-    [featureAttributeName: string]: MapFeatureAttributeState;
-  }): void {
-    buildInitialFeatureAttributeStateNumeric(
-      attributeStatesOfFeatureType,
-      this.name
-    );
-  }
+
   setInitialFilters(
     filterIndexOfAttribute: number,
     stateOfAttribute: MapFeatureAttributeState,
     dispatch: Dispatch<any>
   ): void {
-    setInitialFiltersNumeric(
-      filterIndexOfAttribute,
-      this.name,
-      stateOfAttribute,
-      this.KeplerFilterType,
-      dispatch
+    const attributeName = this.name;
+    dispatch(setFilter(filterIndexOfAttribute, "name", attributeName));
+    dispatch(setFilter(filterIndexOfAttribute, "type", this.KeplerFilterType));
+    stateOfAttribute = stateOfAttribute as MapNumericFeatureAttributeState;
+    dispatch(
+      setFilter(filterIndexOfAttribute, "value", [
+        stateOfAttribute.range!.min,
+        stateOfAttribute.range!.max,
+      ])
     );
+    dispatch(setFilter(filterIndexOfAttribute, "enlarged", false)); //disables any pop-up Kepler may bring up with the filter
+
+    return;
   }
   abstract readonly name: FeatureAttributeName;
   readonly typeOfAttribute = TypeOfFeatureAttribute.NUMBER;
--- app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/classes/StringFeatureAttribute.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/attributeStrategies/classes/StringFeatureAttribute.ts
@@ -1,14 +1,14 @@
 import {FeatureAttribute} from "./FeatureAttribute";
 import {MapFeatureAttributeState} from "../../states/map/MapFeatureAttributeState/MapFeatureAttributeState";
 import {MapFeature} from "../../states/map/MapFeature";
-import {updateStringAttributeStateOfFeatureType} from "../functions/updateAttributeStateOfFeatureType/updateStringAttributeStateOfFeatureType";
 import {buildInitialFeatureAttributeStateString} from "../functions/buildInitialFeatureAtttributeState/buildInitialFeatureAtttributeStateString";
 import {Dispatch} from "redux";
-import {setInitialFiltersString} from "../functions/setInitialFilters/setInitialFiltersString";
 import {FeatureAttributeName} from "../../states/map/FeatureAttributeName";
 import {TypeOfFeatureAttribute} from "../../states/map/TypeOfFeatureAttribute";
 import {KeplerFilterType} from "../../states/map/KeplerFilterType";
 import {KeplerFieldType} from "../../states/map/KeplerFieldType";
+import {MapStringFeatureAttributeState} from "../../states/map/MapFeatureAttributeState/MapStringFeatureAttributeState";
+import {setFilter} from "kepler.gl/actions";
 
 export abstract class StringFeatureAttribute implements FeatureAttribute {
   updateAttributeStatesOfFeatureType(
@@ -17,32 +17,50 @@ export abstract class StringFeatureAttribute implements FeatureAttribute {
     },
     addedFeature: MapFeature
   ): void {
-    updateStringAttributeStateOfFeatureType(
-      attributeStatesOfFeatureType,
-      addedFeature,
-      this.name
-    );
+    const attributeName = this.name;
+    const attributeKey = this.name as keyof MapFeature;
+    //Specifying the attribute state of the addedFeature's FeatureType to be updated (e.g. the state of 'frequency' of Transmissions)
+
+    let attributeStateOfAttributeOfFeatureType = attributeStatesOfFeatureType[
+      attributeName
+    ] as MapStringFeatureAttributeState;
+    //If this is the first time coming across this attribute for the addedFeature's FeatureType
+    if (!attributeStateOfAttributeOfFeatureType) {
+      //Give the attribute a default MapStringFeatureAttributeState
+      attributeStatesOfFeatureType[attributeName] = {
+        values: [],
+        filterIndex: null,
+      };
+      attributeStateOfAttributeOfFeatureType = attributeStatesOfFeatureType[
+        attributeName
+      ] as MapStringFeatureAttributeState;
+    }
+    //add the value of addedFeature's attribute into the values list of the attribute state if the value is not null and is not already included.
+    if (
+      addedFeature[attributeKey] !== null &&
+      !attributeStateOfAttributeOfFeatureType.values.includes(
+        addedFeature[attributeKey] as string
+      )
+    )
+      attributeStateOfAttributeOfFeatureType.values.push(
+        addedFeature[attributeKey] as string
+      );
+
+    return;
   }
-  buildInitialFeatureAttributeState(attributeStatesOfFeatureType: {
-    [featureAttributeName: string]: MapFeatureAttributeState;
-  }): void {
-    buildInitialFeatureAttributeStateString(
-      attributeStatesOfFeatureType,
-      this.name
-    );
-  }
+
   setInitialFilters(
     filterIndexOfAttribute: number,
     stateOfAttribute: MapFeatureAttributeState,
     dispatch: Dispatch<any>
   ): void {
-    setInitialFiltersString(
-      filterIndexOfAttribute,
-      this.name,
-      stateOfAttribute,
-      this.KeplerFilterType,
-      dispatch
-    );
+    const attributeName = this.name;
+    stateOfAttribute = stateOfAttribute as MapStringFeatureAttributeState;
+    dispatch(setFilter(filterIndexOfAttribute, "name", attributeName));
+    dispatch(setFilter(filterIndexOfAttribute, "type", this.KeplerFilterType));
+    dispatch(setFilter(filterIndexOfAttribute, "enlarged", false)); //disables any pop-up Kepler may bring up with the filter
+
+    return;
   }
   abstract readonly name: FeatureAttributeName;
   readonly typeOfAttribute = TypeOfFeatureAttribute.STRING;
