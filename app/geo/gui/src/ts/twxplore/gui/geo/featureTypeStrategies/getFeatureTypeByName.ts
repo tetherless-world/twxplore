e90b062c826ae5ab163045e2b8bb8c0799f4b66b
Covenant <cfaluyi@gmail.com>
2020-05-06 13:19:30 -0400

    Factory method getFeatureTypeByName

---
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/getFeatureTypeByName.ts
@@ -0,0 +1,36 @@
+import {FeatureType} from "../api/graphqlGlobalTypes";
+import {CountyFeatureTypeStrategy} from "./PolygonFeatureTypeStrategy/CountyFeatureTypeStrategy";
+import {StateFeatureTypeStrategy} from "./PolygonFeatureTypeStrategy/StateFeatureTypeStrategy";
+import {MetropolitanDivisionFeatureTypeStrategy} from "./PolygonFeatureTypeStrategy/MetropolitanDivisionFeatureType";
+import {MilitaryInstallationFeatureTypeStrategy} from "./PolygonFeatureTypeStrategy/MilitaryInstallationFeatureTypeStrategy";
+import {TransmissionFeatureTypeStrategy} from "./PointFeatureType/TransmissionFeatureTypeStrategy";
+import {TransmitterFeatureTypeStrategy} from "./PointFeatureType/TransmitterFeatureTypeStrategy";
+
+type FeatureTypeKey = keyof typeof FeatureType;
+
+export const getFeatureTypeByName = (name: string) => {
+  switch (FeatureType[name as FeatureTypeKey]) {
+    case FeatureType.State: {
+      return StateFeatureTypeStrategy.instance;
+    }
+    case FeatureType.County: {
+      return CountyFeatureTypeStrategy.instance;
+    }
+    case FeatureType.MetropolitanDivision: {
+      return MetropolitanDivisionFeatureTypeStrategy.instance;
+    }
+    case FeatureType.MilitaryInstallation: {
+      return MilitaryInstallationFeatureTypeStrategy.instance;
+    }
+    case FeatureType.Transmission: {
+      return TransmissionFeatureTypeStrategy.instance;
+    }
+    case FeatureType.Transmitter: {
+      return TransmitterFeatureTypeStrategy.instance;
+    }
+
+    default: {
+      throw Error("Unrecognized Feature Type");
+    }
+  }
+};
