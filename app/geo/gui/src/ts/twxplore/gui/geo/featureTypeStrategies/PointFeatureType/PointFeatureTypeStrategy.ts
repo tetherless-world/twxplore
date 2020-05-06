4bea8a168c14e9f0b157ebb16f4fa166de32b943
Covenant <cfaluyi@gmail.com>
2020-05-06 13:05:26 -0400

    Created abstract sublcass PointFeatureTypeStrategy with isExpandable = true and empty withinFeatureTypes list

---
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PointFeatureType/PointFeatureTypeStrategy.ts
@@ -0,0 +1,7 @@
+import {FeatureTypeStrategy} from "../FeatureTypeStrategy";
+
+export abstract class PointFeatureTypeStrategy implements FeatureTypeStrategy {
+  abstract readonly name: string;
+  readonly withinFeatureTypes = [];
+  readonly isExpandable = false;
+}
