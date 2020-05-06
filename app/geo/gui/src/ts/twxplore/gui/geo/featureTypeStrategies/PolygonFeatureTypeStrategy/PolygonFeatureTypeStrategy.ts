a05a71194168eaeefdef583875ab0905ce566f74
Covenant <cfaluyi@gmail.com>
2020-05-06 13:00:53 -0400

    Created abstract sublcass PolygonFeatureTypeStrategy with isExpandable = true

---
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PolygonFeatureTypeStrategy/PolygonFeatureTypeStrategy.ts
@@ -0,0 +1,8 @@
+import {FeatureTypeStrategy} from "../FeatureTypeStrategy";
+
+export abstract class PolygonFeatureTypeStrategy
+  implements FeatureTypeStrategy {
+  abstract readonly name: string;
+  abstract readonly withinFeatureTypes: string[];
+  readonly isExpandable = true;
+}
