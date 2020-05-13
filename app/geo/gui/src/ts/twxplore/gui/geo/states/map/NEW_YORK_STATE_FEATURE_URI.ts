aeae5dfbe1ae53f8b86cbf224b8f32aa610a49ab
Covenant <cfaluyi@gmail.com>
2020-05-13 12:37:08 -0400

    Removed unnecessary types from URI types files

--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/NEW_YORK_STATE_FEATURE_URI.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/NEW_YORK_STATE_FEATURE_URI.ts
@@ -1,3 +1,2 @@
-type NEW_YORK_STATE_FEATURE_URI = "http://twxplore.github.io/app/geo/feature#tiger_line-tl_2019_us_state-38";
-export const NEW_YORK_STATE_FEATURE_URI: NEW_YORK_STATE_FEATURE_URI =
+export const NEW_YORK_STATE_FEATURE_URI =
   "http://twxplore.github.io/app/geo/feature#tiger_line-tl_2019_us_state-38";
--- app/geo/gui/src/ts/twxplore/gui/geo/states/map/ROOT_FEATURE_URI.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/states/map/ROOT_FEATURE_URI.ts
@@ -1,3 +1,2 @@
-type ROOT_FEATURE_URI = "http://twxplore.github.io/app/geo/feature#root";
-export const ROOT_FEATURE_URI: ROOT_FEATURE_URI =
+export const ROOT_FEATURE_URI =
   "http://twxplore.github.io/app/geo/feature#root";
