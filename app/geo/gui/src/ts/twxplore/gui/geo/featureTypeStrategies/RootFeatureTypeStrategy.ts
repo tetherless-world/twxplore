cc50fc8528162319de4f832c3eee37e22387da12
Covenant <cfaluyi@gmail.com>
2020-05-15 17:38:58 -0400

    layerConfigChange implemented on RootFeatureTypStrategy

--- app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/RootFeatureTypeStrategy.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/RootFeatureTypeStrategy.ts
@@ -1,12 +1,24 @@
 import {FeatureTypeStrategy} from "./FeatureTypeStrategy";
 import {FeatureType, FeatureQuery} from "../api/graphqlGlobalTypes";
 import {NEW_YORK_STATE_FEATURE_URI} from "../states/map/NEW_YORK_STATE_FEATURE_URI";
+import {Dispatch} from "redux";
+import {FeaturesByType} from "../states/map/FeaturesByType";
 
 export class RootFeatureTypeStrategy implements FeatureTypeStrategy {
   getClickedQuery(clickedFeatureUri: string): FeatureQuery {
     return {
       onlyFeatureUri: NEW_YORK_STATE_FEATURE_URI,
     };
+  }
+  layerConfigChange(
+    keplerLayers: any,
+    layerIndex: number,
+    dispatch: Dispatch<any>,
+    featuresByType: {
+      [featureType: string]: FeaturesByType;
+    }
+  ): void {
+    return;
   }
   readonly name = FeatureType.Root;
   readonly childFeatureTypes = [FeatureType.State];
