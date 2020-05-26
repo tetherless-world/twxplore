f30114e4c84541c7e7cb1de2d39089d0c4308c33
Minor Gordon <gordom6@rpi.edu>
2020-05-26 17:52:02 -0400

    syntax error

--- app/geo/script/load-data.sh
+++ app/geo/script/load-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/../../.."
-java -Dtwks.clientReadTimeoutMs=120000 -jar ../twks/java/dist/twks-cli-current.jar put-nanopublications
+java -Dtwks.clientReadTimeoutMs=120000 -jar ../twks/java/dist/twks-cli-current.jar put-nanopublications \
   data/geo/loaded/dsa/features.ttl.bz2 \
   data/geo/loaded/osn/features.ttl.bz2 \
   data/geo/loaded/tiger_line/features.ttl.bz2 \
