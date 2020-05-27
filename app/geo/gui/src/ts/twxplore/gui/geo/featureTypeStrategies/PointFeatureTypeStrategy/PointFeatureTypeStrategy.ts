1046103d04afe6c649a360ca907cd9f8e5d3ab31
Covenant <cfaluyi@gmail.com>
2020-05-27 11:58:33 -0400

    Fixed imports

--- app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PointFeatureTypeStrategy/PointFeatureTypeStrategy.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PointFeatureTypeStrategy/PointFeatureTypeStrategy.ts
@@ -1,9 +1,6 @@
 import {FeatureTypeStrategy} from "../FeatureTypeStrategy";
 import {FeatureType} from "../../api/graphqlGlobalTypes";
 import {FeatureAttributeName} from "../../states/map/FeatureAttributeName";
-import {Dispatch} from "redux";
-import {FeaturesByType} from "../../states/map/FeaturesByType";
-import {MapFeatureTypeState} from "../../states/map/MapFeatureTypeState";
 import {DispatchLayerConfigurationActionsParameters} from "../DispatchLayerConfigurationActionsParameters";
 
 export abstract class PointFeatureTypeStrategy implements FeatureTypeStrategy {
