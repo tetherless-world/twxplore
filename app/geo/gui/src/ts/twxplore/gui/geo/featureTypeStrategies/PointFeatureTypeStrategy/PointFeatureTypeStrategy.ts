239facdef8d18571b0b9a2c4e0b842361afb0b17
Covenant <cfaluyi@gmail.com>
2020-05-27 11:51:11 -0400

    Replace parameter object with interface

--- app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PointFeatureTypeStrategy/PointFeatureTypeStrategy.ts
+++ app/geo/gui/src/ts/twxplore/gui/geo/featureTypeStrategies/PointFeatureTypeStrategy/PointFeatureTypeStrategy.ts
@@ -4,22 +4,15 @@ import {FeatureAttributeName} from "../../states/map/FeatureAttributeName";
 import {Dispatch} from "redux";
 import {FeaturesByType} from "../../states/map/FeaturesByType";
 import {MapFeatureTypeState} from "../../states/map/MapFeatureTypeState";
+import {DispatchLayerConfigurationActionsParameters} from "../DispatchLayerConfigurationActionsParameters";
 
 export abstract class PointFeatureTypeStrategy implements FeatureTypeStrategy {
   getClickedQuery(clickedFeatureUri: string): null {
     return null;
   }
-  abstract dispatchLayerConfigurationActions(kwds: {
-    keplerLayerOfFeatureType: any;
-    keplerFilterOfFeatureType: any;
-    keplerFieldsOfFeatureType: any;
-    keplerInteractionConfig: any;
-    featureTypeStateOfFeatureType: MapFeatureTypeState;
-    dispatch: Dispatch<any>;
-    featuresByType: {
-      [featureType: string]: FeaturesByType;
-    };
-  }): void;
+  abstract dispatchLayerConfigurationActions(
+    kwds: DispatchLayerConfigurationActionsParameters
+  ): void;
   abstract readonly name: FeatureType;
   readonly childFeatureTypes = [];
   readonly expandable = false;
